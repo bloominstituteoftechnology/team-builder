8735b800cc786ca6a541f499d913ea7f4b7f597a
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 13:45:17 -0700

    Member component renders email now

--- team-builder/src/components/Member.jsx
+++ team-builder/src/components/Member.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Member = (props) => {
-    const {name, age, occupation, favFood} = props.member
+    const {name, age, occupation, favFood, email} = props.member
     return (
         <div>
             <h3>{name}</h3>
             <h5>{occupation}</h5>
             <p>{age}</p>
+            <p>{email}</p>
             <p>{favFood}</p>
             <button onClick={() => props.editMember(name)}>Edit</button>
         </div>
