66545aa823382d70fd8c6832a8aa4af5d16dac48
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:55:41 -0700

    Import use State hook in form component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
 import TeamMemberList from "./views/TeamMemberList";
+import Form from "./components/Form"
 
 function App() {
   const [members, setMembers] = useState([
     { name: "Jeff", age: "26", occupation: "Chef", favFood: "Pasta" },
     { name: "April", age: "20", occupation: "Jr Developer", favFood: "BBQ" },
-    { name: "Devin", age: "43", occupation: "Marketing Director", favFood: "Prime Rib" },
-    { name: "Maddie", age: "31", occupation: "Self - Employed", favFood: "Asparagus" }
+    {
+      name: "Devin",
+      age: "43",
+      occupation: "Marketing Director",
+      favFood: "Prime Rib"
+    },
+    {
+      name: "Maddie",
+      age: "31",
+      occupation: "Self - Employed",
+      favFood: "Asparagus"
+    }
   ]);
-  return <div className="App" >
-    <TeamMemberList members={members} />
-  </div>;
+  return (
+    <div className="App">
+      <TeamMemberList members={members} />
+      <Form />
+    </div>
+  );
 }
 
 export default App;
