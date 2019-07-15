c7d1608522669bb87890c53682e4743be327ef3c
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:56:59 -0700

    Change handler add to all inputs

--- team-builder/src/components/Form.jsx
+++ team-builder/src/components/Form.jsx
@@ -1,20 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Form = () => {
-    return (
-        <form>
-            <label>Name</label>
-            <input type="text" placeholder="name"/>
-            <label>Age</label>
-            <input type="number" />
-            <label htmlFor="">Email</label>
-            <input type="text" placeholder="email"/>
-            <label htmlFor="">Occupation</label>
-            <input type="text"/>
-            <label htmlFor="">Favorite Food</label>
-            <input type="text" />
-        </form>
-    );
+const Form = props => {
+  const changeHandler = e => {};
+  return (
+    <form>
+      <label>Name</label>
+      <input type="text" placeholder="name" onChange={changeHandler} />
+      <label>Age</label>
+      <input type="number" onChange={changeHandler} />
+      <label htmlFor="">Email</label>
+      <input type="text" placeholder="email" onChange={changeHandler} />
+      <label htmlFor="">Occupation</label>
+      <input type="text" onChange={changeHandler} />
+      <label htmlFor="">Favorite Food</label>
+      <input type="text" onChange={changeHandler} />
+    </form>
+  );
 };
 
 export default Form;
