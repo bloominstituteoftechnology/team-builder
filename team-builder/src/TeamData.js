df8d01d9a50c512f34c2eb7e9f5eff0e948d1c2c
Elise Spain <elisespain@gmail.com>
2019-07-15 16:22:43 -0700

    Team data update

---
+++ team-builder/src/TeamData.js
@@ -0,0 +1,82 @@
+export const teamData = [
+    {
+        first_name:"Elise",
+        last_name: "Spain",
+        postion: "Student",
+        hobby: "Lettering"
+
+    },
+
+    {
+        first_name:"Mel",
+        last_name: "Harris",
+        postion: "Student",
+        hobby: "Art"
+
+    },
+
+    {
+        first_name:"Greg",
+        last_name: "Jordan",
+        postion: "Student",
+        hobby: "Reading"
+
+    },
+
+    {
+        first_name:"Tyrone",
+        last_name: "Smith",
+        postion: "Student",
+        hobby: "Sports"
+
+    },
+
+    {
+        first_name:"Kesha",
+        last_name: "Lake",
+        postion: "Job Search",
+        hobby: "Music"
+
+    },
+    {
+        first_name:"Jenn",
+        last_name: "Weaver",
+        postion: "School Prep",
+        hobby: ""
+
+    },
+
+    {
+        first_name:"Levar",
+        last_name: "Morris",
+        postion: "Student",
+        hobby: "Tech"
+
+    },
+
+    {
+        first_name:"Ben",
+        last_name: "Early",
+        postion: "PT Student",
+        hobby: "Music"
+
+    },
+
+    {
+        first_name:"Lizzy",
+        last_name: "Early",
+        postion: "PT Student",
+        hobby: "Music"
+
+    },
+
+    {
+        first_name:"Jen",
+        last_name: "Carney",
+        postion: "PT Student",
+        hobby: "Volunteer Work"
+
+    },
+
+
+]
