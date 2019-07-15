e7197d7f13023ba5ceaa9d376bfac62265d362b5
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 13:05:50 -0700

    Add names to inputs

--- team-builder/src/components/Form.jsx
+++ team-builder/src/components/Form.jsx
@@ -1,27 +1,53 @@
 import React, { useState } from "react";
 
 const Form = props => {
-
-    const [formState, setFormState] = useState({
-        name: '',
-        age: '',
-        email: '',
-        occupation: '',
-        favFood: ''
-    })
+  const [formState, setFormState] = useState({
+    name: "",
+    age: "",
+    email: "",
+    occupation: "",
+    favFood: ""
+  });
   const changeHandler = e => {};
   return (
     <form>
       <label>Name</label>
-      <input type="text" placeholder="name" onChange={changeHandler} value={formState.name}/>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={changeHandler}
+        value={formState.name}
+        name="name"
+      />
       <label>Age</label>
-      <input type="number" onChange={changeHandler} value={formState.age}/>
+      <input
+        type="number"
+        onChange={changeHandler}
+        value={formState.age}
+        name="age"
+      />
       <label htmlFor="">Email</label>
-      <input type="text" placeholder="email" onChange={changeHandler} value={formState.email}/>
+      <input
+        type="text"
+        placeholder="email"
+        onChange={changeHandler}
+        value={formState.email}
+        name="email"
+      />
       <label htmlFor="">Occupation</label>
-      <input type="text" onChange={changeHandler} value={formState.occupation}/>
+      <input
+        type="text"
+        onChange={changeHandler}
+        value={formState.occupation}
+        name="occupation"
+      />
       <label htmlFor="">Favorite Food</label>
-      <input type="text" onChange={changeHandler} value={formState.favFood}/>
+      <input
+        type="text"
+        onChange={changeHandler}
+        value={formState.favFood}
+        name="favFood"
+      />
     </form>
   );
 };
