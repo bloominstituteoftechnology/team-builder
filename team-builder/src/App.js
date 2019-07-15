d683c1f7b2d56c1ad0defac5410245252bcf2a4a
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 13:44:42 -0700

    Add email to initial state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,44 +1,57 @@
 import React, { useState } from "react";
 import "./App.css";
 import TeamMemberList from "./views/TeamMemberList";
-import Form from "./components/Form"
+import Form from "./components/Form";
 
 function App() {
   const [members, setMembers] = useState([
-    { name: "Jeff", age: "26", occupation: "Chef", favFood: "Pasta" },
-    { name: "April", age: "20", occupation: "Jr Developer", favFood: "BBQ" },
+    {
+      name: "Jeff",
+      age: "26",
+      occupation: "Chef",
+      favFood: "Pasta",
+      email: "test123@yahoo.com"
+    },
+    {
+      name: "April",
+      age: "20",
+      occupation: "Jr Developer",
+      favFood: "BBQ",
+      email: "turlesarepeopletoo@hotmail.com"
+    },
     {
       name: "Devin",
       age: "43",
       occupation: "Marketing Director",
-      favFood: "Prime Rib"
+      favFood: "Prime Rib",
+      email: "blahblah@gmail.com"
     },
     {
       name: "Maddie",
       age: "31",
       occupation: "Self - Employed",
-      favFood: "Asparagus"
+      favFood: "Asparagus",
+      email: "yoo@gmail.com"
     }
   ]);
   const [edit, setEdit] = useState({
     edit: false,
     member: {}
-  })
+  });
 
-  const addMember = (e) => {
-    setMembers([...members, e])
-  }
+  const addMember = e => {
+    setMembers([...members, e]);
+  };
   const editMember = name => {
-    const member = members.find(mem => mem.name === name)
-    setEdit({edit: true, member: member})
+    const member = members.find(mem => mem.name === name);
+    setEdit({ edit: true, member: member });
     // console.log("edit", member)
-  }
-  console.log(edit)
+  };
+  console.log(edit);
   return (
-    
     <div className="App">
-      <TeamMemberList members={members} editMember={editMember}/>
-      <Form addMember={addMember} edit={edit}/>
+      <TeamMemberList members={members} editMember={editMember} />
+      <Form addMember={addMember} edit={edit} />
     </div>
   );
 }
