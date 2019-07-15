6e12c22631b4a5515ea96ea350d0724bc55f459a
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 13:00:29 -0700

    Input values now controlled by state

--- team-builder/src/components/Form.jsx
+++ team-builder/src/components/Form.jsx
@@ -13,15 +13,15 @@ const Form = props => {
   return (
     <form>
       <label>Name</label>
-      <input type="text" placeholder="name" onChange={changeHandler} />
+      <input type="text" placeholder="name" onChange={changeHandler} value={formState.name}/>
       <label>Age</label>
-      <input type="number" onChange={changeHandler} />
+      <input type="number" onChange={changeHandler} value={formState.age}/>
       <label htmlFor="">Email</label>
-      <input type="text" placeholder="email" onChange={changeHandler} />
+      <input type="text" placeholder="email" onChange={changeHandler} value={formState.email}/>
       <label htmlFor="">Occupation</label>
-      <input type="text" onChange={changeHandler} />
+      <input type="text" onChange={changeHandler} value={formState.occupation}/>
       <label htmlFor="">Favorite Food</label>
-      <input type="text" onChange={changeHandler} />
+      <input type="text" onChange={changeHandler} value={formState.favFood}/>
     </form>
   );
 };
