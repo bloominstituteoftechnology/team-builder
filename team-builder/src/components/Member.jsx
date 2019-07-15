3f8939214649ffb52e85df8e76b75fe792fefa6f
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:47:28 -0700

    Import Member in List component

---
+++ team-builder/src/components/Member.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Member = (props) => {
+    return (
+        <div>
+            <h3></h3>
+            <h5></h5>
+            <p></p>
+            <p></p>
+        </div>
+    );
+};
+
+export default Member;
--- team-builder/src/views/TeamMemberList.js
+++ team-builder/src/views/TeamMemberList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Member from '../components/Member'
 
 const TeamMemberList = (props) => {
     return (
