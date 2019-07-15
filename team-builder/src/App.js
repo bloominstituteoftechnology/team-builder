70a08e16f8900ddac157bb9a24bced6c78088fe8
Elise Spain <elisespain@gmail.com>
2019-07-15 12:55:38 -0700

    Init commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HI!!!</h1>
+        
+      
+    </div>
+  );
+}
+
+export default App;
