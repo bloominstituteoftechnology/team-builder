676fb8de2a3ddf56d39cdee2c80231b7a6e61574
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:49:19 -0700

    Members are rendering to page

--- team-builder/src/components/Member.jsx
+++ team-builder/src/components/Member.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const Member = (props) => {
+    const {name, age, occupation, favFood} = props.member
     return (
         <div>
-            <h3></h3>
-            <h5></h5>
-            <p></p>
-            <p></p>
+            <h3>{name}</h3>
+            <h5>{occupation}</h5>
+            <p>{age}</p>
+            <p>{favFood}</p>
         </div>
     );
 };
