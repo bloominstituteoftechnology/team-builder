a4de17648f433540f2ddc41708f40e5b69e1b10c
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:46:11 -0700

    Lopp through each member and pass it to Member component

--- team-builder/src/views/TeamMemberList.js
+++ team-builder/src/views/TeamMemberList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TeamMemberList = (props) => {
     return (
         <div>
-            {props.members.map((member, i) => <Member key={i} />)}
+            {props.members.map((member, i) => <Member key={i} member={member}/>)}
         </div>
     );
 };
