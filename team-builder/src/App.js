6d7caa1d4390e3d51b5dd7034a202b98d5a81fb2
Michael Harris <mhguitar1997@gmail.com>
2019-07-15 12:33:04 -0700

    Create initial state list for team members

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-
-  const [members, setMembers] = useState([])
+  const [members, setMembers] = useState([
+    { name: "Jeff", age: "26", occupation: "Chef", favFood: "Pasta" },
+    { name: "April", age: "20", occupation: "Jr Developer", favFood: "BBQ" },
+    { name: "Devin", age: "43", occupation: "Marketing Director", favFood: "Prime Rib" },
+    { name: "Maddie", age: "31", occupation: "Self - Employed", favFood: "Asparagus" }
+  ]);
   return <div className="App" />;
 }
 
