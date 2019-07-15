6188dcf5e9c725502dc955a9aedf8e5c05106b74
Ian Be <ian84be@gmail.com>
2019-07-15 17:16:17 -0500

    implement edit & remove functions

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import 'semantic-ui-css/semantic.min.css'
 function App() {
   const [team, setTeam] = useState([{id:1,email:'steve@dave.com',name:'Steve',role:'Front End Developer'}]);
   const [newMem, setNewMem] = useState({email:'',name:'',role:''});
+  const [memberToEdit, setMemberToEdit] = useState({email:'',name:'',role:''});
   const [editNum, setEditNum] = useState(0);
   
   return (
@@ -38,7 +39,18 @@ function App() {
 
             <Grid.Column textAlign='justified' verticalAlign='middle'>
               <Card.Group>
-                {team.map(mem => <TeamCard {...mem} editNum={editNum} toggleEdit={toggleEdit}/>)}
+                {team.map(mem => 
+                  <TeamCard 
+                    {...mem} 
+                    editHandler={editHandler}
+                    editMember={editMember} 
+                    editNum={editNum} 
+                    memberToEdit={memberToEdit}
+                    toggleEdit={toggleEdit}
+                    key={mem.id}
+                    removeMember={removeMember}
+                  />
+                )}
               </Card.Group>
             </Grid.Column>
           </Grid>
@@ -51,13 +63,35 @@ function App() {
   function changeHandler(e) {
     setNewMem({...newMem, [e.target.name]:e.target.value});
   }
+
+  function editHandler(e) {
+    setMemberToEdit({...memberToEdit, [e.target.name]:e.target.value});
+  }
+
   function addNewMember(e) {
     e.preventDefault();
     setTeam([...team, {...newMem, id:team.length+1}]);
     setNewMem({email:'',name:'',role:''})
   }
+
+  function editMember(e, memberToEdit) {
+    e.preventDefault();
+    let newTeam = team.filter(mem => mem.id !== memberToEdit.id);
+    setTeam([...newTeam, memberToEdit]);
+    setEditNum(0);
+  }
+
+  function removeMember(e, id) {
+    e.preventDefault();
+    let newTeam = team.filter(mem => mem.id !== id);
+    setTeam(newTeam);
+    setEditNum(0);
+  }
+
   function toggleEdit(id) {
     editNum === id ? setEditNum() : setEditNum(id);
+    let thisMem = team.filter(mem => mem.id === id)[0];
+    setMemberToEdit(thisMem);
   }
 }
 
--- client/src/TeamCard.js
+++ client/src/TeamCard.js
@@ -1,24 +1,50 @@
-import React, {useState} from 'react';
-import {Button,Card} from 'semantic-ui-react';
+import React, {useState,useEffect} from 'react';
+import {Button,Card,Form} from 'semantic-ui-react';
 
 function TeamCard (props) {
+    const [thisMember, setThisMember] = useState({email:'',name:'',role:''});
+
+    useEffect(() => {
+        setThisMember(props.memberToEdit);
+    },[props.memberToEdit])
+    
     return (
         <Card>
             {props.editNum === props.id ? (
             <>
                 <Card.Content>
-                    <Card.Header>{props.name}</Card.Header>
-                    <Card.Meta>{props.email}</Card.Meta>
-                    <Card.Description>{props.role}</Card.Description>
+                <Form>
+                    <Form.Input
+                    label='Name'
+                    name='name'
+                    value={thisMember.name}
+                    onChange={(e) => editHandler(e)}
+                    size='mini'
+                    />
+                    <Form.Input
+                    label='Email'
+                    name='email'
+                    value={thisMember.email}
+                    onChange={(e) => editHandler(e)}
+                    size='mini'
+                    />
+                    <Form.Input
+                    label='Role'
+                    name='role'
+                    value={thisMember.role}
+                    onChange={(e) => editHandler(e)}
+                    size='mini'
+                    />
+                </Form>
                 </Card.Content>
                 <Card.Content extra>
                     <div className='ui two buttons'>
-                    <Button color='yellow' onClick={() => props.toggleEdit(props.id)}>
+                    <Button basic color='yellow' onClick={() => props.toggleEdit(props.id)}>
                         Cancel
                     </Button>
-                    {/* <Button color='red'>
-                        Remove
-                    </Button> */}
+                    <Button color='green' onClick={(e) => props.editMember(e, thisMember)}>
+                        Submit
+                    </Button>
                     </div>
                 </Card.Content>
             </>
@@ -34,7 +60,7 @@ function TeamCard (props) {
                     <Button color='yellow' onClick={() => props.toggleEdit(props.id)}>
                         Edit
                     </Button>
-                    <Button color='red'>
+                    <Button color='red' onClick={(e) => props.removeMember(e, props.id)}>
                         Remove
                     </Button>
                     </div>
@@ -42,7 +68,11 @@ function TeamCard (props) {
             </>
             )}
         </Card>
-    )
+    );
+
+    function editHandler(e) {
+        setThisMember({...thisMember, [e.target.name]:e.target.value});
+    }
 }
 
 export default TeamCard;
