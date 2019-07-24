c323f7fa9616131d8d41a5d18f34a7322cf79dfa
Kelly M <moreira.kd@outlook.com>
2019-07-24 15:41:36 -0400

    some of part1

---
+++ team-builder/src/TeamData.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+
+export const TeamData = [
+
+
+    {   Name: 'Ross Geller',
+        Occupation: 'Professor',
+        Hobby: 'Dinosaurs'
+    },
+
+
+    {   Name: 'Monica Geller',
+        Occupation: 'Chef',
+        Hobby: 'Cleaning'
+    },
+
+    {   Name: 'Chandler Bing',
+        Occupation: 'Transponder',
+        Hobby: 'Sarcasim'
+    },
+
+    {   Name: 'Joey Tribianni',
+        Occupation: 'Actor',
+        Hobby: "Lady's man"
+    },
+
+    {   Name: 'Pheebe Buffet',
+        Occupation: 'Masseuse',
+        Hobby: 'Composing innapropriate music'
+    },
+
+    {   Name: 'Rachel Greene',
+        Occupation: 'Buyer',
+        Hobby: 'Fashion'
+    },
+
+
+
+]
