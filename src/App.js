c578b9482246f27a05bd4f6e9d9f7864cd54865f
dangolbeeker <scottb305@outlook.com>
2019-07-24 18:42:53 -0400

    form displaying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from "./Components/Form"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [teamArray, setTeamArray] = useState([])
+  const [memberToEdit, setMemberToEdit] = useState({})
+
+  const editMember = (member) => {
+    console.log("member going to edit", member)
+    setMemberToEdit(member)
+    console.log("app memberToEdit", memberToEdit)
+  }
+
+  console.log("app teamArray", teamArray)
+  if (teamArray.length === 0) {
+    return (
+      <div className="App">
+        <Form
+          setTeamArray={setTeamArray}
+          teamArray={teamArray}
+          memberToEdit={memberToEdit}
+        />
+        <div>
+        Add a new team member
+      </div>
     </div>
-  );
+    )
+    } else {
+    return (
+      <div className="App">
+      <Form  
+        setTeamArray={setTeamArray}
+        teamArray={teamArray}
+      />
+      <div>
+      {teamArray.map(member => {
+        console.log("member", member)
+          return (
+            <div key={member.email}>
+              <teamMembers
+                member={member}
+              />
+              <button
+                onClick={() => editMember(member)}
+              >
+                edit
+              </button>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+    )
+  }
 }
 
 export default App;
