4eb34a50a27e06fb7aecbac50551d25b8e3bfbb0
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-07-24 16:10:13 -0500

    still on step one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import { teamData } from './TeamData';
 
-function App() {
+
+const App = () => {
+  const [memberList, setMemberList] = useState( [teamData] );
+
+  const addToSavedList = movie => {
+    setSavedList( [...savedList, movie] );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SavedList list={savedList} />
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/:id" render={ (props) => {
+        return(<Movie {...props} />)
+      }} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TeamData.js
@@ -0,0 +1,17 @@
+export const teamData = [
+    {
+        name: "John Payne",
+        email: "john.payne@gmail.com",
+        role: "Cheif Executive Officer"
+    }, 
+    {
+        name: "Vanesa Fleurs",
+        email: "vanesa.fleurs@gmail.com",
+        role: "Cheif Financial Officer"
+    }, 
+    {
+        name: "Victor Hugo",
+        email: "victor.hugo@gmail.com",
+        role: "Chief Information Officer"
+    }
+]
