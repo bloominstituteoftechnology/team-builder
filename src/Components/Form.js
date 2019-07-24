7ab49d551f438b50cc8e2c0aebe693fd943c39ac
dangolbeeker <scottb305@outlook.com>
2019-07-24 16:44:07 -0400

    building out form component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,4 +1,34 @@
 import React, { useState } from 'react';
 
-import logo from './logo.svg';
-import './App.css';
+
+function Form(props) {
+
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        role: "",
+    })
+
+    const [member, setMember] = useState ({
+        name: "",
+        email: "",
+        role: "",
+    })
+
+const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("submitHandler formState", formState)
+    console.log("teamArray", props.teamArray)
+    props.setTeamArray([
+        ...props.teamArray,
+        formState
+    ])
+    setFormState({
+        name: "",
+        email: "",
+        role: "",
+    })
+
+    console.log(teamArray)
+
+}
