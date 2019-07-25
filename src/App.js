9b683108201ca418c198c94ee4e20c3cbdf3b93e
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-25 07:49:29 -0500

    set up simple page structure in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1> The Llambda Llamas</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Join Us!</h2>
+        </div>
+        <div className="flex-large">
+          <h2>Meet the Team</h2>
+        </div>
+      </div>
     </div>
   );
 }
