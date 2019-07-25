0a8537a416c5da4220d93bd8fe5a9c14327a0660
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-07-25 08:56:37 -0500

    mapped user data to list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import MemberList from './UserData';
 
 function App() {
+  const membersData = [
+    {id: 1, name: 'Britt', email: 'britt@brittcodes.com', role: 'Queen of Everything'},
+    {id: 2, name: 'Angela', email: 'alasalle@llambda.com', role: 'Savior of the Baby Devs'}
+  ]
+  
+  const [members, setMembers] = useState(membersData);
+  
   return (
     <div className="container">
       <h1> The Llambda Llamas</h1>
@@ -10,6 +18,7 @@ function App() {
         </div>
         <div className="flex-large">
           <h2>Meet the Team</h2>
+          <MemberList members={members}/> 
         </div>
       </div>
     </div>
--- src/UserData.js
+++ src/UserData.js
@@ -1,26 +1,39 @@
 import React from 'react';
 
-const MemberList = () => {
-    <table>
+
+
+function MemberList(props) {
+   
+    return(
+   <table>
         <thead>
             <tr>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>Name data</td>
-                <td>email data</td>
-                <td>
-                    <button className='button muted-button'>Edit</button>
-                    <button className='button muted-button'>Delete</button>
+            {props.members.map(member => (
+                    <tr key ={member.id}>
+                        <td>{member.name}</td>
+                        <td>{member.email}</td>
+                        <td>{member.role}</td>
+                        <td>
+                         <button className='button muted-button'>Edit</button>
+                         <button className='button muted-button'>Delete</button>
 
-                </td>
-            </tr>
+                        </td>
+                    </tr>
+                ))
+             
+             }
+            
         </tbody>
     </table>
-} 
+   )
+            }
+
 
 export default MemberList
