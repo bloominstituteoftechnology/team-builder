f36befe4abcc1506cfa70b47084ade52aade5913
spettigrew <sara.pettigrew5@gmail.com>
2019-09-12 20:19:36 -0600

    "logos deleted"

--- src/Form.js
+++ src/Form.js
@@ -50,3 +50,71 @@ const Forms = (props) => {
 export default Forms;
 
 // (...name) === spread operator, will include all that's within the useState() operator. {}object, []array, ""string, etc. 
+
+
+
+// import React, { useState } from "react";
+
+// function LoginForm() {
+//     const [user, setUser] = useState({ username: "", password: "" });
+
+//     const handleChange = event => {
+//         setUser({ ...user, [event.target.name]: event.target.value });
+//     };
+
+//     const handleSubmit = event => {
+//         event.preventDefault();
+//         console.log(user.username);
+//         console.log(user.password);
+//     };
+
+//     return (
+//         <div className="loginForm">
+//             {console.log(user)}
+//             <form onSubmit={event => handleSubmit(event)}>
+//                 <label>
+//                     Username:
+//           <input
+//                         type="text"
+//                         name="username"
+//                         onChange={event => handleChange(event)}
+//                     />
+//                 </label>
+//                 <label>
+//                     Password:
+//           <input
+//                         type="text"
+//                         name="password"
+//                         onChange={event => handleChange(event)}
+//                     />
+//                 </label>
+//                 <button>Submit!</button>
+//             </form>
+//         </div>
+//     );
+// }
+
+// export default LoginForm;
+
+
+// After install of formick
+// import { withFormik, Form, Field } from "formik";
+// <Form>
+//     <label>
+//         Username:
+//     <input
+//             type="text"
+//             name="username"
+//             onChange={event => handleChange(event)}
+//         />
+//     </label>
+//     <label>
+//         Password:
+//     <input
+//             type="text"
+//             name="password"
+//             onChange={event => handleChange(event)}
+//         />
+//     </label>
+//     <button>Submit!</button>
+// </Form>
