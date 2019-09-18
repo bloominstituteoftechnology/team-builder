84a2ae552c4cf965cd77829ab1ecff32c53264d9
meera-andersen <meera.andersen@gmail.com>
2019-09-18 16:36:11 -0600

    added some color and card border.

--- team-builder/src/components/TeamMembers.js
+++ team-builder/src/components/TeamMembers.js
@@ -4,9 +4,9 @@ import React from 'react';
 const TeamMembers = props => {
 
     return (
-    <div>
+    <div className = "card-container">
         {props.teamMembers.map(teamMember => (
-            <div key={teamMember.id}>
+            <div className ="team-member" key={teamMember.id}>
                 <h2>{teamMember.name}</h2>
                 <p>{teamMember.role}</p>
                 <p>{teamMember.email}</p>
