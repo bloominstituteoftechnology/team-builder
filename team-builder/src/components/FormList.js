57e6330f43f9be31654b9d439f5dff1804893104
Afia-Lydia <you@example.com>
2019-09-18 19:36:56 -0400

    update

--- team-builder/src/components/FormList.js
+++ team-builder/src/components/FormList.js
@@ -1,16 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+border: 4px solid teal;
+width:500px;
+height: 300px;
+`
 
 function FormList(props){
     return(
         <div>
             {
                 props.members.map((member, index)=>(
-                    <div className="listItem" key={index}>
+                    <Container className="listItem" key={index}>
                         <h2>{member.memberName}</h2>
                         <p>{member.email}</p>
                         <p>{member.role}</p>
 
-                    </div>
+                    </Container>
                 ))
             }
 
