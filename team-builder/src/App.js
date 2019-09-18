773ec113b9660f6089a5a3d75ea3519f36653bfe
admin <admin@admins-MacBook-Pro.local>
2019-09-18 10:53:47 -0700

    initial commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello this is the main page
+    </div>
+  );
+}
+
+export default App;
