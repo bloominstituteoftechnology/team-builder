4a37c29ae2717dfc1d7b6a593414fb6e43220e48
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-18 17:03:18 +0100

    created divs and put my label and input tags

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,33 +1,58 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import App from './App'
 
 const initialFormValues = {
-    name:'1',
-    age:'2',
-    role:'3',
+    name: '1',
+    age: '2',
+    role: '3',
 };
 
 
 
-function Form () {
+function Form() {
     // passed in initialFormValues to the useState so that my state variable would use it as the initial value
-   const [formValues, setFormValues] = useState(initialFormValues);
+    const [formValues, setFormValues] = useState(initialFormValues);
 
+    const onNameChange = (e) => {
+        setFormValues({
+            ...formValues,
+            name:e.target.value,
+        })
+    }
 
-     return (<form>
-         <label htmlFor='nameinput'>Name</label>
-         <input
-         value={formValues.name}/>
+    const onAgeChange = (e) => {
+        setFormValues({
+            ...formValues,
+            age:e.target.value,
+        })
+    }
 
-         <label htmlFor='ageInput'>Age</label>
-         <input
-         value={formValues.age}/>
+    const onRoleChange = (e) => {
+        setFormValues({
+            ...formValues,
+            role:e.target.value,
+        })
+    }
 
-       <label htmlFor='roleInput'>Role</label>
-         <input
-         value={formValues.role}/>
-     </form>
-     )
+    return (<form>
+        <label htmlFor='nameinput'>Name</label>
+        <input
+            value={formValues.name}
+            onChange={onNameChange}
+             />
+
+        <label htmlFor='ageInput'>Age</label>
+        <input
+        
+         value={formValues.age}
+         onChange={onAgeChange} />
+
+        <label htmlFor='roleInput'>Role</label>
+        <input
+            value={formValues.role}
+            onChange={onRoleChange} />
+    </form>
+    )
 }
 
 export default Form;
