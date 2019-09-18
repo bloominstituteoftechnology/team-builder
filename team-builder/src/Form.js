0b1fba9b33ec7649173aba5abfec07596f0e5a9e
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-18 15:19:23 +0100

    created initial form values and passed it to the usestate so that it sets to the initial value of my usestate

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,2 +1,19 @@
 import React, {useState} from 'react';
 
+const initialFormValues = {
+    name:'1',
+    email:'2',
+    role:'3',
+};
+
+
+
+function Form () {
+    // passed in initialFormValues to the useState so that my state variable would use it as the initial value
+   const [formValues, setFormValues] = useState(initialFormValues);
+
+
+    return
+}
+
+export default Form;
