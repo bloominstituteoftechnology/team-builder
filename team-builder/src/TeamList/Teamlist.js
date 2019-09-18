559ffecd3c8aeea0cb305e166a3fac22539ad801
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-18 13:57:49 +0100

    give state value and pass in list of team members

--- team-builder/src/TeamList/Teamlist.js
+++ team-builder/src/TeamList/Teamlist.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import React, { useState } from 'react';
 
+const initialTeamList = [
+    { id: uuid(), name: 'Maryam', age: 20},
+    { id: uuid(), name: 'Kiyani', age: 20},
+    { id: uuid(), name: 'Mildred', age: 20},
+]
 
 function TeamList () {
-    const [teamMemberList, setTeamMemberList] = useState();
+    const [teamMemberList, setTeamMemberList] = useState(initialTeamList);
 }
 
 export default TeamList;
