d5e2ca6b3fb07af08b05301fc2eeb5e83dd6afc0
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-18 15:08:54 +0100

    created a form.js and imported usestate

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import uuid from 'uuid';
 import './App.css';
 
-const initialTeamMembers = [ 
-  { id: uuid(), name: 'Amira', age: '20', role: 'Product Manager'},
-  { id: uuid(), name: 'Dom', age: '21', role: 'Technical Lead'},
-  { id: uuid(), name: 'Martins', age: '23', role: 'Software Developer'},
-  { id: uuid(), name: 'Francis', age: '24', role: 'Product Sponsor'},
-  { id: uuid(), name: 'Abel', age: '22', role: 'Software Tester'},
-  { id: uuid(), name: 'Babatunde', age: '26', role: 'Subject Matter Expert'},
+const initialTeamMembers = [
+  { id: uuid(), name: 'Amira', age: '20', role: 'Product Manager' },
+  { id: uuid(), name: 'Dom', age: '21', role: 'Technical Lead' },
+  { id: uuid(), name: 'Martins', age: '23', role: 'Software Developer' },
+  { id: uuid(), name: 'Francis', age: '24', role: 'Product Sponsor' },
+  { id: uuid(), name: 'Abel', age: '22', role: 'Software Tester' },
+  { id: uuid(), name: 'Babatunde', age: '26', role: 'Subject Matter Expert' },
 ];
 
-const [teamMembers, setTeamMembers] = useState(initialTeamMembers);
 
 function App() {
+
+  // passed in initialTeamMembers to the useState so that my state variable would use it as the initial value
+  const [teamMembers, setTeamMembers] = useState(initialTeamMembers);
+
+
   return (
     <div className="App">
     </div>
