7eea6bb90ec9e4f690c7462fbbf2f0ac27bdb9fa
kiyani <bamba.kiyani@gmail.com>
2019-09-19 15:36:15 -0400

    MVP completed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -8,82 +8,101 @@ import uuid from 'uuid';
 
 const initialteamMemberList = [
   {id: uuid(), name: 'Kiyani', email: 'kiyani@lambda.com', role: 'Product manager'},
-  {id: uuid(), name: 'Raissa', email: 'raissa@lambda.com', role: 'Growth engineer'},
+  {id: uuid(), name: 'Raissa', email: 'raissa@shopify.com', role: 'Growth engineer'},
   {id: uuid(), name: 'Maelys', email: 'maelys@paystack.com', role: 'UX engineer'},
   {id: uuid(), name: 'Monique', email: 'monique@loreal.com', role: 'Chemical engineer'},
 ]
    
 const initialTeamMemberForm = {
-name:'KIYANI',
-email:'',
-role:'',
+  name:'',
+  email:'',
+  role:'',
 }
 
-// { ...TeamMemberForm, [event.target.name]: event.target.value }
 
 function App() {
   const [teamMemberList, setteamMemberList] = useState(initialteamMemberList);
-  const [TeamMemberForm, setTeamMemberForm] = useState (initialTeamMemberForm);
+  const [teamMemberForm, setTeamMemberForm] = useState (initialTeamMemberForm);
     
   const handleChange = event => {
-    setTeamMemberForm ([{name: event.target.value, email:event.target.value, role:event.target.value}]);
+    console.log(event.target.name);
+    setTeamMemberForm({...teamMemberForm, [event.target.name]: event.target.value});
   };
     
 
   // when the user clicks submit the state will be reset with empty strings
   const handleSubmit = event => {
-  event.preventDefault();
-
-  setteamMemberList(teamMemberList.concat(TeamMemberForm));
+    event.preventDefault();
+    setteamMemberList(teamMemberList.concat(teamMemberForm));
   };
 
   return (
     <div className="App">
 
-    {/* {console.log(teamMemberList)} */}
-    
-    { 
-      teamMemberList.map(members => (
-      <h4 key={members.id}>
-        {members.name}, {members.role}. Contact : {members.email}.
-        </h4>
-      ))
-    }
+      {/* {console.log(teamMemberList)} */}
+      
+      { 
+        teamMemberList.map(member => (
+          <Member key={member.id} mem={member} />
+        ))
+      }
+      <Form handleSubmit={handleSubmit} handleChange={handleChange} teamMemberForm={teamMemberForm}/>
+   
+    </div>
+  );
+}
+      
+export default App;
+
+
+const Form = props => {
+  const  { handleSubmit, handleChange, teamMemberForm} = props;
+
+  return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Name:
+        <label>
+          Name:
+          <input 
+            type='text'
+            name='name'
+            value={teamMemberForm.name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label>
+        Email:
+        <input 
+        type='email'
+        name='email'
+        value={teamMemberForm.email}
+        onChange={event => handleChange(event)}
+        />
+        </label>
+
+        <label>
+        Role:
         <input 
         type='text'
-        name='name'
-        value={TeamMemberForm.name}
+        name='role'
+        value={teamMemberForm.role}
         onChange={event => handleChange(event)}
         />
       </label>
 
-      <label>
-      Email:
-      <input 
-      type='text'
-      email='email'
-      value={TeamMemberForm.email}
-      onChange={event => handleChange(event)}
-      />
-      </label>
-
-      <label>
-      Role:
-      <input 
-      type='text'
-      role='role'
-      value={TeamMemberForm.role}
-      onChange={event => handleChange(event)}
-      />
-    </label>
-
-    <button> Submit </button>
+      <button> Submit </button>
     </form>
-    </div>
-  );
- }
-      
-export default App;
+
+  )
+}
+
+
+const Member = props => {
+  const { id, name, role, email } = props.mem;
+
+  return (
+    <h4>
+      {name}, {role}. Contact : {email}.
+    </h4>
+  )
+}
