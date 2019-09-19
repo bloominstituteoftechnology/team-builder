c1442affaf95d40d9910796706415c51f22d4b4e
kiyani <bamba.kiyani@gmail.com>
2019-09-19 10:27:38 -0400

    modified OnChange

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -6,67 +6,76 @@ import uuid from 'uuid';
 // uuid : creating unique ID for every object
  
 
-const initialTeamMemberList = [
+const initialteamMemberList = [
   {id: uuid(), name: 'Kiyani', email: 'kiyani@lambda.com', role: 'Product manager'},
   {id: uuid(), name: 'Raissa', email: 'raissa@lambda.com', role: 'Growth engineer'},
   {id: uuid(), name: 'Maelys', email: 'maelys@paystack.com', role: 'UX engineer'},
   {id: uuid(), name: 'Monique', email: 'monique@loreal.com', role: 'Chemical engineer'},
 ]
    
-// const initialTeamMemberForm = {
-// name:'',
-// email:'',
-// role:'',
-// }
+const initialTeamMemberForm = {
+name:'KIYANI',
+email:'',
+role:'',
+}
 
+// { ...TeamMemberForm, [event.target.name]: event.target.value }
 
 function App() {
-  const [TeamMemberList, setTeamMemberList] = useState(initialTeamMemberList);
-  // const [TeamMemberForm, setTeamMemberForm] = useState (initialTeamMemberForm);
+  const [teamMemberList, setteamMemberList] = useState(initialteamMemberList);
+  const [TeamMemberForm, setTeamMemberForm] = useState (initialTeamMemberForm);
     
   const handleChange = event => {
-    setTeamMemberList ({ ...TeamMemberList, [event.target.name]: event.target.value });
+    setTeamMemberForm ([{name: event.target.value, email:event.target.value, role:event.target.value}]);
   };
     
+
+  // when the user clicks submit the state will be reset with empty strings
   const handleSubmit = event => {
   event.preventDefault();
+
+  setteamMemberList(teamMemberList.concat(TeamMemberForm));
   };
-    
-  
+
   return (
     <div className="App">
-    {
-      TeamMemberList.map(members => (
+
+    {/* {console.log(teamMemberList)} */}
+    
+    { 
+      teamMemberList.map(members => (
       <h4 key={members.id}>
         {members.name}, {members.role}. Contact : {members.email}.
         </h4>
-      )) 
+      ))
     }
-
-    <form onSubmit={event => handleSubmit(event)}>>
+    <form onSubmit={handleSubmit}>
       <label>
-          Name:
-        <input type='text'
+        Name:
+        <input 
+        type='text'
         name='name'
-        value={TeamMemberList.name}
+        value={TeamMemberForm.name}
         onChange={event => handleChange(event)}
         />
       </label>
 
       <label>
       Email:
-      <input type='text'
+      <input 
+      type='text'
       email='email'
-      value={TeamMemberList.email}
+      value={TeamMemberForm.email}
       onChange={event => handleChange(event)}
       />
       </label>
 
       <label>
       Role:
-      <input type="text"
+      <input 
+      type='text'
       role='role'
-      value={TeamMemberList.role}
+      value={TeamMemberForm.role}
       onChange={event => handleChange(event)}
       />
     </label>
