0cca2d32fb0b3aad8f75d24adb7be388c7bba10b
kiyani <bamba.kiyani@gmail.com>
2019-09-18 23:39:16 -0400

    Built form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// uuid : creating unique ID for every object
+ 
+
+const initialTeamMemberList = [
+  {id: uuid(), name: 'Kiyani', email: 'kiyani@lambda.com', role: 'Product manager'},
+  {id: uuid(), name: 'Raissa', email: 'raissa@lambda.com', role: 'Growth engineer'},
+  {id: uuid(), name: 'Maelys', email: 'maelys@paystack.com', role: 'UX engineer'},
+  {id: uuid(), name: 'Monique', email: 'monique@loreal.com', role: 'Chemical engineer'},
+]
+   
+const initialTeamMemberForm = {
+name:'',
+email:'',
+role:'',
 }
 
-export default App;
+
+function App() {
+  const [TeamMemberList, setTeamMemberList] = useState(initialTeamMemberList);
+  const [TeamMemberForm, setTeamMemberForm] = useState (initialTeamMemberForm);
+    
+  const handleChange = event => {
+    setTeamMemberList ({ ...TeamMemberList, [event.target.name]: event.target.value });
+  };
+    
+      // const handleSubmit = event => {
+      //   event.preventDefault();
+      //   console.log(members.name);
+      // };
+    
+      return (
+        <div className="App">
+
+          <form>
+            <label>
+              Name:
+              <input type="text"
+              name="name"
+              value={TeamMemberList.name}
+              onChange={event => handleChange(event)}
+              />
+            </label>
+
+            <label>
+              Email:
+              <input type="text"
+              email="name"
+              value={TeamMemberList.email}
+              onChange={event => handleChange(event)}
+              />
+            </label>
+
+            <label>
+              Role:
+              <input type="text"
+              role="name"
+              value={TeamMemberList.role}
+              onChange={event => handleChange(event)}
+              />
+            </label>
+
+            {
+             TeamMemberList.map ( members => (
+               <h4 key = {members.id}>
+                 {members.name}, {members.role}. Contact : {members.email}.
+               </h4>
+             )) 
+            }
+            <button> Submit </button>
+          </form>
+        </div>
+      );
+    }
+    
+    export default App;
