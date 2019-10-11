7bca99f8ba07a7552580bfc88fe7ed622e555cbd
Brian Hague <alphaseinor@gmail.com>
2019-10-10 20:42:51 -0500

    added state for users, added addUser function with UUID for id

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {uuid} from 'uuid';
 
 function App() {
+
+  const [users, setUsers] = useState();
+
+  const addUser = user => {
+    const newUser = {
+      id: uuid.v4(),
+      name: user.name,
+      email: user.email,
+      role: user.role
+    }
+  }
+
   return (
     <div className="App">
       
