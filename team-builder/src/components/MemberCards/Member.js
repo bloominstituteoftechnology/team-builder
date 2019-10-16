4dc340bef4ac2a0e92af0aa7bd28413da7a26f78
Mark Halls <mark.halls@gmail.com>
2019-10-16 15:10:58 -0700

    Add labels to card details

--- team-builder/src/components/MemberCards/Member.js
+++ team-builder/src/components/MemberCards/Member.js
@@ -3,9 +3,9 @@ import React from "react";
 const Member = props => {
   return (
     <div>
-      <p>{props.card.name}</p>
-      <p>{props.card.email}</p>
-      <p>{props.card.role}</p>
+      <p>Name: {props.card.name}</p>
+      <p>Email: {props.card.email}</p>
+      <p>Role: {props.card.role}</p>
     </div>
   );
 };
