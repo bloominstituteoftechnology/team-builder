426a0873cd82e7ab0e1a9ac18d257f3d0602d7fe
Trey McGarity <trey.mcgarity99@gmail.com>
2019-11-04 19:50:27 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Form from './components/Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Form() {
+    return (
+      <div className="form-container">
+        <form>
+            <label>The Form</label>
+        </form>
+      </div>
+    );
+  }
+  export default Form;
