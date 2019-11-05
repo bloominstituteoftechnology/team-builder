baa837f819fae524a334a2d9af1bd45733c09626
chiragthesia <harithesia@gmail.com>
2019-11-04 20:39:42 -0600

    Created a forms file

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [teamMember, setTeamMember] = useState(" ");
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ team-builder/src/Components/Forms.js
@@ -0,0 +1,2 @@
+import React, { useState } from 'react';
+import './App.css';
