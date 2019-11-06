9da01c0da1fa0610e3a324fa3d16b16ea42219c4
Samuel Kraus <S_read@aol.com>
2019-11-05 18:46:11 -0600

    set up useState, began constructing teammembers section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMembers from './Components/TeamMembers';
+import data from "./data"
+import Form from './Components/Form';
 
 function App() {
+
+  const [notes, setNotes] = useState(data)
+
+  const addNewNote = note => {
+    setNotes([...notes, note])
+  }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Form addNewNote={addNewNote}/>
+      <TeamMembers team ={notes}/>
     </div>
   );
 }
