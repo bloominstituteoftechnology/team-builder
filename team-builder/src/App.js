81909067d7385b064a9cfa67af9e99276de85688
cappers86 <52895127+cappers86@users.noreply.github.com>
2019-11-13 13:52:11 +0000

    added team members inital state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,27 +1,19 @@
 import React, { useState } from 'react';
-import ReactDom from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+  const [members, setMembers] = useState([
+
+    {name:"Dave", email: "dave@thisemail.com", role:"Bus driver",},
+    {name:"Mike", email: "mike@thisemail.com", role:"Banker",},
+    {name:"Bill", email: "bill@thisemail.com", role:"Programmer",}
+  ]);
+  
+  
+  
+  
+  return <div className="App" />;	  
+}	
 
 export default App;
