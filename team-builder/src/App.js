c8273b0217dc86ca1c5b92429a0369dcdfc955ee
jarrodskahill <jarrodskahill@gmail.com>
2019-11-13 09:31:50 -0800

    implemented yarn, and started building team list

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Team Builder</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Components/TeamList.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+
+function Team() {
+    const [TeamMember, setTeamMember] = useState({
+        name:"",
+        email:"",
+        role:""
+    });
+    return(
+        <div></div>
+    );
+}
+
+export default Team
