c90d60c8b58362d8ab934dac62cfef271409dcc9
Edwin Chajon <junchajo@gmail.com>
2019-11-14 12:27:44 -0800

    App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
-import Form from './components/Form';
-import TeamList from './components/TeamList';
 import './App.css';
 
 function App() {
-  const [members, setMembers] = useState([]);
+  //applying a useState hook and setting a state peoperty to members
+ const [members, setMembers] = useState([]);
 
-  const addMember = (event, name, email, role) => {
-    event.preventDefault();
-  
+  const addMember = (e, name, email, role) => {
+    e.preventDefault();
+
+    //defining member. The id is predetermined by a timestamp
     const member = {
       id: Date.now(),
-      name,
+      name, 
       email,
-      role,
+      role
     }
 
-    setMembers([...members, member]);
-  };
+    //What will apply changes to member
+    setMembers([...members, member])
+  }
 
-  return (
+  //return the components that will be displayed 
+  // Form is the input that will receive information (email, name, role)
+  // TeamList will display the input information from Form
+  return(
     <div className = "Team">
       <h1>Team</h1>
-      <Form addMember = {addMember} />
-      <TeamList members = {members} />
+      <Form addMember = {addMember}/>
+      <TeamList members ={members}/>
     </div>
   )
 }
