0e3d6b991753a29d01c0e65ef88686ce8c20a941
taty2010 <shydalys123@gmail.com>
2019-12-10 14:44:40 -0600

    added useState and passed down person prop for TeamMember

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMember from './Components/TeamMember'
 
 function App() {
+  const [person, setPerson] = useState([
+    {
+        id: 1,
+        firstName:'Doctor',
+        lastName:'Who' 
+    }
+]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Team Member List</h1>
       </header>
+      <div>
+        <TeamMember person={person} />
+      </div>
     </div>
   );
 }
