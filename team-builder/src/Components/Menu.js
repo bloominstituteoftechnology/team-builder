eaf71982858a5ac59da11af3636f894ba61671de
taty2010 <shydalys123@gmail.com>
2019-12-10 15:09:54 -0600

    added the Menu component for the nav

---
+++ team-builder/src/Components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Menu = () =>{
+    return(
+        <div className="menu">
+            <h2>Home</h2>
+            <h2>About Us</h2>
+            <h2>Team</h2>
+        </div>
+    )
+}
+
+export default Menu
