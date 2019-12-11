bcf4e4b07c7babe46f50a02108254de7487c7da1
taty2010 <shydalys123@gmail.com>
2019-12-10 19:22:42 -0600

    added the role and email props

--- team-builder/src/Components/TeamMember.js
+++ team-builder/src/Components/TeamMember.js
@@ -5,8 +5,10 @@ const TeamMember = props =>{
     return(
         <div className='teamList'>
             {props.person.map(person =>(
-                <div className='person'>
+                <div key={person.id} className='person'>
                     <h2>{`${person.firstName}  ${person.lastName}`}</h2>
+                    <h3>{person.role}</h3>
+                    <p>{person.email}</p>
                 </div>
             ))
 
