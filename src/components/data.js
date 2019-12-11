d530a1f80b773b37889a4187ccf2205ef11f2ae0
alberto90rodriguez <arodriguez.jr@outlook.com>
2019-12-10 18:52:15 -0600

    added index.html to public folder

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+const teamData = [
+    {
+    id: 1,
+    name: 'Junior Rodriguez',
+    email: 'junior@email.com',
+    role: 'Back-End Engineer'
+    },
+    {
+    id: 2,
+    name: 'Jasmin Rodriguez',
+    email: 'jasmin@email.com',
+    role: 'Front-End Engineer'
+    },
+    {
+    id: 3,
+    name: 'Arianna Rodriguez',
+    email: 'arianna@email.com',
+    role: 'UX design'
+    }
+]
+
+export default teamData;
