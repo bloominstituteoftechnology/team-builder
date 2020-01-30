47a4d93cb91575cf289bb23521d3bbb08a050d7f
mastaryan <sizemore.ryan@gmail.com>
2020-01-30 18:18:26 -0500

    Team Members Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMembers from './Components/TeamMembers';
+import data from "./data"
+import Form from './Components/Form';
 
 function App() {
+  const [notes, setNotes] = useState(data)
+
+  const addNewNote = note => {
+    const addNewNote = note => {
+      setNotes([...notes, note])
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
