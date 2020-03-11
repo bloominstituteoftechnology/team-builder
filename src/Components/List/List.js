460d4aeb673e7824f5338a74651725d4543fb711
yirano <yirano@protonmail.ch>
2020-03-10 23:58:18 -0400

    Render team list

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const List = (props) => {
+  console.log(props.data);
+  console.log('name', props.data.name);
   return (
     <div>
-      <h1>{props.user.name}</h1>
-      <h2>{props.user.email}</h2>
-      <h3>{props.user.role}</h3>
+      {props.data.map(user => {
+        return (
+          <div>
+            <h1>{user.name}</h1>
+            <h2>{user.email}</h2>
+            <h3>{user.role}</h3>
+            <h4>Hi</h4>
+          </div>
+        )
+
+      })
+      }
     </div>
   )
 }
