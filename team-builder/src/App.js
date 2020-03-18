14a7db6720d0df7456d03e6b75d0dbcfdc319811
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-17 20:16:45 -0400

    starting off need to remember to commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <label>
+          Username:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default App;
