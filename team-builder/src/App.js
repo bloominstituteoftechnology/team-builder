f3ce317a5c97e9d80a9a63e18739889883be4410
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-17 21:31:24 -0400

    almost there i think

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState({username:"", password:""});
-
-  
+  const [user, setUser] = useState({ username: "", password: "" });
 
   const handleChange = event => {
-    setUser({...user, {event.target.name}: event.target.value});
+    setUser({ ...user, [event.target.name]: event.target.value });
   };
 
-  
   const handleSubmit = event => {
     event.preventDefault();
     console.log(user.name);
@@ -20,23 +17,25 @@ function App() {
   return (
     <div className="App">
       {console.log(user)}
-      
 
-      <form onSubmit={event =>handleSubmit(event)}>
+      <form onSubmit={event => handleSubmit(event)}>
         <label>
           Username:
           <input
-           type="text"
-           name= "username"
-           value= {user.username}
-            onChange={event => handleChange(event)} />
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={event => handleChange(event)}
+          />
         </label>
         <label>
           Password:
-          <input type="text"
-          name="password"
-          value= {user.password}
-           onChange={event=> handleChange(event)}/>
+          <input
+            type="text"
+            name="password"
+            value={user.password}
+            onChange={event => handleChange(event)}
+          />
         </label>
         <button>Submit</button>
       </form>
