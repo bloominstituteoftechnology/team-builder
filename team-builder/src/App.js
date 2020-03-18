7da2c2c54ae0000422cb1185943fe06f873768a6
azrael3069 <cmrivera0004@student.stcc.edu>
2020-03-17 20:40:32 -0400

    coming along, commit

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,22 +1,38 @@
-import React, { usestate } from "react";
-
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [name, setName] = useState("");
 
-  const handleChange = event => {
+  const[password,setPassword]=useState("");
+
+  const handleNameChange = event => {
     setName(event.target.value);
+  };
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(name);
+    console.log(password);
   };
 
   return (
     <div className="App">
-      {console.log(name)}
-      <form>
+      {console.log({name})}
+      {console.log({password})}
+
+      <form onSubmit={event =>handleSubmit(event)}>
         <label>
           Username:
-          <input type="text" onChange={event => handleChange(event)} />
+          <input type="text" onChange={event => handleNameChange(event)} />
         </label>
+        <label>
+          Password:
+          <input type=="text" onChange={event=> handlePasswordChange(event)}/>
+        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
