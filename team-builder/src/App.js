cbb9bbc4f3bcf836cf574ab3e563f8e4f02dba25
isaac stott <isaac.cloyd@gmail.com>
2020-04-21 14:54:08 -0600

    setup app js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,8 +2,28 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MemberForm from './MemberForm'
+import Member from './Member'
 
 function App() {
+  const [memberValues, setMemberValues] = useState([
+    {
+      name: 'John Doe',
+      email: 'i-shot-the-sheriff@gmail.com',
+      role: 'Man on the Run'
+    }
+  ])
+
+  const addNewMember = member => {
+    const newMember = {
+      name: member.name,
+      email: member.email,
+      role: member.role
+    }
+    setMemberValues([...memberValues, newMember]);
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +31,11 @@ function App() {
         <h1>Add New Member</h1>
       </header>
 
-      <MemberForm/>
+      <MemberForm addNewMember={addNewMember} />
 
+      <div className="form">
+        <Member memberValues={memberValues} />
+      </div>
     </div>
   );
 }
