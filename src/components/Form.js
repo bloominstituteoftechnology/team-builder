c39f0e14ac2f867fce8c45e6566d8deadc2ae6aa
Jason Ours <jasonours@outlook.com>
2020-04-26 22:52:49 -0500

    wording change

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ const Form = ({addNewName}) => {
         role: "",
     })
 
-    const changeHandler = event => {
+    const handleChange = event => {
         setTeamMembers({...teamMembers,
             [event.target.name]: event.target.value
         })
@@ -23,11 +23,11 @@ const Form = ({addNewName}) => {
         <Input>
             <form onSubmit={submit}>
                 <Label>Name:</Label>
-                <Type onChange={changeHandler} type="text" name="name" value={teamMembers.name} />
+                <Type onChange={handleChange} type="text" name="name" value={teamMembers.name} />
                 <Label>Email Address:</Label>
-                <Type onChange={changeHandler} type="email" name="email" value={teamMembers.email} />
+                <Type onChange={handleChange} type="email" name="email" value={teamMembers.email} />
                 <Label>Role:</Label>
-                <Type onChange={changeHandler} type="text" name="role" value={teamMembers.role} />
+                <Type onChange={handleChange} type="text" name="role" value={teamMembers.role} />
                 <Button type="submit">Submit</Button>
             </form> 
         </Input>
