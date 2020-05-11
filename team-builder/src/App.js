34a57d7032cb96134c39b33f8158e46a0c018e44
Cassandra Dew <cpdew2@gmail.com>
2020-05-11 19:25:39 -0400

    project started

---
+++ team-builder/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  const [team, setTeam] = useState([]);
+
+  const addMember = member => {
+    setTeam([...team, member]);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      
+     
+      
+      )
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function Form(props) {
+    const [value, setValue] = useState ({name: "", email:"", role: ""});
+
+    const handleChange = event => {
+        event.persist();
+        setValue(() => ({ ...value, [event.target.name]: event.target.value}));
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log("This should update the state in App with", value);
+        props.addMember(value);
+        setValue({ name: "", email: "", role: ""});
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={value.name}
+                onChange={handleChange}
+            />
+                <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={value.name}
+                onChange={handleChange}
+            />
+                <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={value.name}
+                onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
