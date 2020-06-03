5aa85c7b3c56636af1579ae22f46710d55dbb1e9
tomytank <github@iamtom.com>
2020-06-03 18:09:15 +0200

    Added submit handler

--- src/Form.js
+++ src/Form.js
@@ -3,11 +3,22 @@ import React from 'react';
 
 const Form = () => {
 
+const submitHandler = event => {
+    event.preventDefault();
+    console.log("Fomr props");
+    PaymentResponse.
+}
 
 
 return(
     <div>
         <p>Form is here</p>
+    <form 
+    onSubmit={submitHandler}
+    >
+
+    </form>
+    
     </div>
 );
 
