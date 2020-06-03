9b4c4e938ce68d01afd3f37c1b06379dc2fd76f6
tomytank <github@iamtom.com>
2020-06-03 13:07:07 +0200

    State setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import Form from './Form';
+
 import './App.css';
 
+// const team; 
+
+
 function App() {
+
+  const [team, setTeam] = useState("team");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Form />
           Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Form = () => {
+
+
+
+return(
+    <div>
+        <p>Form is here</p>
+    </div>
+);
+
+}
+
+export default Form; 
