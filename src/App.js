c39b842a199c6d27e8af38f6e502842eba387238
JenVest2020 <jenvest2019@gmail.com>
2020-06-05 13:34:17 -0500

    skeleton form built and rendered

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import TeamData from './TeamData';
+import Form from './Form';
 import './App.css';
+// import styled from 'styled-components';
+
+// const AppDiv = styled.div`
+//   border: 2px dotted lightblue;
+// `;
 
 function App() {
   const members = TeamData;
   const [team, setTeam] = useState(members);
   return (
-    <div className="App">
+    <div className='App'>
+      <Form />
       {members.map(member => {
         return <div>
           <h1>{member.name}</h1>
--- src/Form.js
+++ src/Form.js
@@ -1,3 +1,15 @@
 import React, { useState } from 'react';
 import TeamData from './TeamData';
 
+const Form = () => {
+    return (
+        <form>
+            <input type='text' placeholder='name' name='name' />
+            <input type='email' placeholder='email' name='email' />
+            <input type='text' placeholder='role' name='role' />
+            <input type='submit' />
+        </form>
+    )
+};
+
+export default Form;
