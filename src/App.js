e33c84ce21e1dc96a2697d65aa98c4a420af2368
JenVest2020 <jenvest2019@gmail.com>
2020-06-05 13:02:59 -0500

    Step 1 completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
+import TeamData from './TeamData';
 import './App.css';
 
 function App() {
+  const members = TeamData;
+  const [team, setTeam] = useState(members);
   return (
     <div className="App">
+      {members.map(member => {
+        return <div>
+          <h1>{member.name}</h1>
+          <p>{member.email}</p>
+          <p>{member.role}</p>
+        </div>;
+      })}
 
     </div>
   );
---
+++ src/TeamData.js
@@ -0,0 +1,59 @@
+const TeamData = [
+    {
+        id: '1',
+        name: 'Charlie Brown',
+        email: 'Cbrown@gmail.com',
+        role: 'frontend engineer',
+    },
+    {
+        id: '2',
+        name: 'Snoopy',
+        email: 'Sdog@gmail.com',
+        role: 'backend engineer'
+    },
+    {
+        id: '3',
+        name: 'Woodstock',
+        email: 'Wbird@gmail.com',
+        role: 'backend engineer',
+
+    },
+    {
+        id: '4',
+        name: 'Linus',
+        email: 'Lblanket@gmail.com',
+        role: 'Design Manager',
+    },
+    {
+        id: '5',
+        name: 'Lucy',
+        email: 'Lucygal@gmail.com',
+        role: 'Program Manager',
+    },
+    {
+        id: '6',
+        name: 'Sally',
+        email: 'Slovebird@gmail.com',
+        role: 'frontend engineer',
+    },
+    {
+        id: '7',
+        name: 'Schroeder',
+        email: 'Smusical@gmail.com',
+        role: 'Video researcher',
+    },
+    {
+        id: '8',
+        name: 'Peppermint Patty',
+        email: 'Patcat@gmail.com',
+        role: 'team lead',
+    },
+    {
+        id: '9',
+        name: 'Marcie',
+        email: 'Marzwoman@gmail.com',
+        role: 'section lead',
+    },
+];
+
+export default TeamData;
