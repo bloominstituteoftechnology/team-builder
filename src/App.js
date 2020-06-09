9b23901b546d99391965484d4c1139a858149720
larwat <lwtech20@gmail.com>
2020-06-08 18:18:21 -0700

    complete

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Form from "./components/Form";
+
+function App() {
+  const [team, setTeam] = useState([]);
+
+  const addMember = member => {
+    setTeam([...team, member]);
+  };
+
+  return (
+    <div className="App">
+      <h2>These are our team members:</h2>
+      {team.map(member => (
+        <div key={member.email}>
+          <p>{member.name} : {member.email} : {member.role}</p>
+         </div>
+      ))}
+      <Form addMember={addMember} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function Form(props) {
+    const [value, setValue] = useState ({name: "", phone:"", role: ""});
+
+    const handleChange = event => {
+        event.persist();
+        setValue(() => ({ ...value, [event.target.name]: event.target.value}));
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log("This should update the state in App with", value);
+        props.addMember(value);
+        setValue({ name: "", phone: "", role: ""});
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={value.name}
+                onChange={handleChange}
+            />
+                <input
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={value.email}
+                onChange={handleChange}
+            />
+                <input
+                type="text"
+                name="role"
+                placeholder="Role"
+                value={value.role}
+                onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
