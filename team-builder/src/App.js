11d1c3e0c4785d532e858a28197470640eba96f1
EmilyLow <emlow@me.com>
2020-07-10 20:40:06 -0500

    Basic structure

---
+++ team-builder/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  const [teamList, setTeamList] = useState("");
+  
+  
+  
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+const testData = [];
+export default App;
