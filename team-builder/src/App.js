1f7742277fecafaa4c2a13cbb60c3b316f124c7c
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-07-12 16:38:18 -0700

    useState and starting Form,js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,28 +1,21 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+import Form from './Form'
 import './App.css';
 
 
-
 function App() {
+  
+  const [members, setMembers] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="App">
+        <Form members={members} setMembers={setMembers}/>
+        <Members members={members}/>
+      </div>
+    );
+  }
+
+
 
 export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Form = props => 
+
+
+
+
+export default Form;
