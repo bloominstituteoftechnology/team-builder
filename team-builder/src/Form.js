204c80f15c9868dfbdfd7db31348014d93433b7f
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-07-12 17:47:18 -0700

    met MVP

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -9,7 +9,48 @@ const Form = props => {
 
 const [newForm, setNewForm] = React.useState(defaultState);
 
+const handleChange = event => {
+    // console.log(event.target.name);
+    console.log(event.target.value);
+    setNewForm({
+      ...newForm,
+      [event.target.name]: event.target.value
+    });
+  };
 
+  const handleSubmit = event => {
+    event.preventDefault();
+    props.setMembers([...props.members, newForm]);
+    setNewForm(defaultState);
+    console.log("submitted!");
+  };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={handleChange}
+        type="text"
+        name="name"
+        value={newForm.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={handleChange}
+        type="text"
+        name="email"
+        value={newForm.email}
+        placeholder="Email"
+      />
+      <input
+        onChange={handleChange}
+        type="text"
+        name="role"
+        value={newForm.role}
+        placeholder="Role"
+      />
+      <button type="submit">Add Member</button>
+    </form>
+  );
+};
 
 export default Form;
