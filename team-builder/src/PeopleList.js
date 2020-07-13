f6d8f62cfd7d4cb4cdcdc854fb93e835b8ac51d8
EmilyLow <emlow@me.com>
2020-07-13 12:49:17 -0500

    Very basic styling

--- team-builder/src/PeopleList.js
+++ team-builder/src/PeopleList.js
@@ -8,10 +8,10 @@ const PeopleList = props => {
         console.log("list", props.list),
         
         //For this, why does props.list.map work, but not props.list.forEach?
-        <div>
+        <div className="peopleList">
            <h1>Team Members:</h1>
-            {props.list.map( (person) => (
-                <div>
+            {props.list.map( (person, i) => (
+                <div className="personCard" key={i}>
                      <p>Name: {person.name}</p>
                      <p>Email: {person.email}</p>
                      <p>Role: {person.role}</p>
