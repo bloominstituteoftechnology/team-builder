281295e297995fed8f826bc5cfb32b9fa76df94b
noah niesen <noah@noahs-MacBook-Pro.local>
2020-07-14 20:21:29 -0500

    made a slight change in the key in Members.js

--- team-builder/src/Members.js
+++ team-builder/src/Members.js
@@ -3,8 +3,8 @@ import React from "react";
 const Members = props => {
   return (
     <div className="member-list">
-      {props.members.map(member => (
-        <div className="member" key={member.id}>
+      {props.members.map((member, i) => (
+        <div className="member" key={i}>
           <h2>{member.name}</h2>
           <p>{member.email}</p>
           <p>{member.role}</p>
