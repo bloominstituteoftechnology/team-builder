a0a0176a6f783d91f0ea6308a5b1ab0115945a38
Christopher Barrett <municipalshock@gmail.com>
2020-09-15 12:51:31 -0700

    address infinite loop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// const teamList = [
+//   { name: "Carl", email: "carl@carl.com", role: "student" },
+//   { name: "tim", email: "tim@tim.com", role: "instructor" },
+//   { name: "sarah", email: "sarah@sarah.com", role: "Team Lead" },
+// ];
+
+// const initialFormValues = {
+//   name: "",
+//   email: "",
+//   role: "",
+// };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [team, setTeam] = useState([]);
+
+  // const [formValues, setFormValues] = useState(initialFormValues);
+  return null;
+  // return (
+  //   // <div className="App">
+  //   //   <h2>Hello World</h2>
+  //   // </div>
+  // );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const teamList = [
+  { name: "Carl", email: "carl@carl.com", role: "student" },
+  { name: "tim", email: "tim@tim.com", role: "instructor" },
+  { name: "sarah", email: "sarah@sarah.com", role: "Team Lead" },
+];
+
+const initialFormValues = {
+  name: "",
+  email: "",
+  role: "",
+};
+
+function SimpleForm() {
+  const [team, setTeam] = useState([]);
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const submit = (evt) => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+    setTeam([...team, newTeamMember]);
+    setFormValues(initialFormValues);
+  };
+
+  return (
+    <div className="App">
+      <h2>Hello World</h2>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <SimpleForm />{" "}
+  </>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
