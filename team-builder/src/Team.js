42c856a1a33e986cbe0c1154f57450e6d9218253
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-10-04 16:25:48 -0700

    created array

---
+++ team-builder/src/Team.js
@@ -0,0 +1,20 @@
+
+
+    const team = [{
+      Name: "Joe",
+      Birthday: 01/01/01,
+      Job:"Plumber"
+    },
+    {
+        Name: "Cam",
+        Birthday: 07/27/94,
+        Job:"Manager"
+      },
+      {
+        Name: "Austin",
+        Birthday: 12/07/95,
+        Job:"Estimator"
+      }];
+
+      export default team
+
