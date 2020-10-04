b2a98ef8173bf82ba384b10b36b7e61f9747d162
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-10-04 16:37:47 -0700

    exported array to app

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,15 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
+import {team}  from './Team';
 import './App.css';
 
 function App() {
-const [data, setData] = useState('');
+const [data, setData] = useState(team);
+console.log(data);
 
   return (
     <div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* <div>{data}</div> */}
     </div>
   );
 }
--- team-builder/src/Team.js
+++ team-builder/src/Team.js
@@ -1,20 +1,20 @@
 
 
-    const team = [{
+ export const team = [{
       Name: "Joe",
-      Birthday: 01/01/01,
+      Birthday: "01/01/01",
       Job:"Plumber"
     },
     {
         Name: "Cam",
-        Birthday: 07/27/94,
+        Birthday: "07/27/94",
         Job:"Manager"
       },
       {
         Name: "Austin",
-        Birthday: 12/07/95,
+        Birthday: "12/07/95",
         Job:"Estimator"
       }];
 
-      export default team
+
 
