94ce04442cd3fb30d466302c1d7d5e03674eeb3f
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-11-01 19:31:25 -0500

    Form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from "./Form.js"
 
 function App() {
+
+  const [member, setMember] = useState([
+    {
+      id: 1,
+      name: '',
+      jobDescription: ''
+    }
+  ]);
+
+  const addNewMember = note => {
+    const newMember = {
+      id: Date.now(),
+      name: note.name,
+      jobDescription: ,
+    }
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Form />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
