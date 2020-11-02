51ba17a515c77d8b4a2c25d2f2b31bdfd9a0de49
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-11-02 15:59:23 -0500

    completed mvp

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,32 @@
 
+import React, {useState} from 'react';
+import Card from './components/Card';
+import Form from './components/MemberForm';
 import './App.css';
 
+
 function App() {
+  const [list, setList] = useState([])
+
+  const addMember = (member) => {
+    const newMember = {
+      id: Date.now(),
+      name: member.name,
+      email: member.email,
+      role: member.role,
+      info: member.info
+    }
+
+    setList([newMember, ...list])
+  }
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <div className="container">
+        <h1>Add a member</h1>
+        <Form addMember={addMember}/>
+        <Card cards={list}/>
+      </div>
     </div>
   );
 }
