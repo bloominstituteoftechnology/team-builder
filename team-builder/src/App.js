816b4e77b9ff1f8b64d0f61f40ac32846836d092
RemyVila <josephvila123@gmail.com>
2020-11-09 18:49:14 -0500

    package-lock.json stuff

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import {Route, Link, Switch} from 'react-router-dom';
 
-function App() {
+
+function App(props) {
+  const [member, setMember] = useState([]);
+
+  // useEffect(() => {
+  //   fetchMember().then((resMember) => setMember(resMember.data));
+  // }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +26,19 @@ function App() {
         >
           Learn React
         </a>
+        {/* <Switch>
+        <Route path={"/items-list/:itemID"}>
+          <Item items={stock} />
+        </Route>
+        <Route path="/items-list">
+          <ItemsList items={stock} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch> */}
+
+        
       </header>
     </div>
   );
