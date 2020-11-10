70bd435095ee801126e8bbd8668e093c74429904
CerritoCode0101 <CaseyCerrito@gmail.com>
2020-11-10 15:34:01 -0600

    finished with Forms

---
+++ src/components/Data.js
@@ -0,0 +1,27 @@
+const theTeam = [
+
+
+    {
+
+      name: 'Iron Man',
+      role: 'Inventor',
+      email: 'avenger1@gmail'
+
+    },
+    {
+
+      name: 'Thor',
+      role: 'God of Thunder',
+      email: 'avenger1@gmail'
+
+    },
+    {
+
+      name: 'Captain America',
+      role: 'Captain',
+      email: 'avenger1@gmail'
+    },
+
+  ];
+
+ export default theTeam 
