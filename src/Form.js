de271e025f71231cb60a83d257db355f32653d8d
Adham AbdelFattah <adham.adel@gmail.com>
2020-11-11 07:54:57 -0600

    morning push

--- src/Form.js
+++ src/Form.js
@@ -4,8 +4,31 @@ export default function Form (props) {
 
     return (
         <form className='join-form'>
-            <input id='input-name' type="text" name="name" placeholder='First Name'/>
-            <input id='input-name-button' type="submit" value="Join" />
+            <label htmlFor='fnameInput'> First Name 
+                <input id='fname' onChange={ evt => {
+                    debugger
+                }
+
+                }type="text" name="name" placeholder='First Name'/>
+            </label>
+            
+            <label>
+                Role: <select id='roleSelect'>
+                    <option > CEO </option>
+                    <option> CPO </option>
+                    <option> VP of Ops </option>
+                    <option> Director of Programs </option>
+                </select>
+            </label>
+            ß
+            <label>
+                <input id='inputButton' type="submit" value="Join" />
+            </label>
+
+            <label>
+                Interested? <input id='interested' type='checkbox'></input>
+            </label>
+            
         </form>
     )
 }
