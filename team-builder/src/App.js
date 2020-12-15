d61061266241d3ec09a136f8b22f8a5d70ee7c78
Brian Reisman <brianreisman@protonmail.com>
2020-12-15 15:25:21 -0500

    initial commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import data from './data';
+console.log(data);
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    name: "Brian",
+    age: 30,
+    role: "student",
+  },
+  {
+    name: "Bill",
+    age: 72,
+    role: "Janitor",
+  },
+  {
+    name: "Beth",
+    age: 41,
+    role: "Artistic Director",
+  },
+];
+export default data;
