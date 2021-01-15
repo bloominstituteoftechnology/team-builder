0cd5f23e23ee95ece6f7d21d862407743799e586
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-14 20:13:18 -0700

    initiated project using npx and created form.js and dummydata.js components

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Team Builder App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
