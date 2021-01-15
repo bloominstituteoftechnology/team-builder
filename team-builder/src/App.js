a1c944a80677c07d972cdbcd069752e7b76c1c51
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-14 20:48:39 -0700

    form.js complete

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import TeamForm from './Form'
 
 import './App.css';
 
 function App() {
+  
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Team Builder App</h1>
+        
       </header>
     </div>
   );
---
+++ team-builder/src/Form.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import teamData from './DummyData'
+
+const data = teamData;
+
+console.log(data)
