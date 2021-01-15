4a32a8ba76aeb3de6143a418ebe8372f364ce836
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-14 20:59:49 -0700

    created team.js component

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,6 +1,55 @@
 import React from 'react'
-import teamData from './DummyData'
 
-const data = teamData;
 
-console.log(data)
+export default function TeamForm(props) {
+
+    const { values, update, submit } = props
+
+    const onChange = e => {
+
+        const name = e.target.name;
+
+        const value = e.target.value;
+
+        update(name, value);
+    }
+
+    const onSubmit = e => {
+
+        console.log('Testing submit func')
+        e.preventDefault();
+
+        submit();
+
+    }
+
+    return (
+        <form className='container' onSubmit={onSubmit}>
+        <div className='form-inputs'>
+            <label>username
+                <input
+                    name='username'
+                    type='text'
+                    placeholder='please enter username here'
+                    maxLength='35'
+                    value={values.username}
+                    onChange={onChange} />
+            </label>
+            
+            <label>Role
+                <select value={values.role} name='role' onChange={onChange}>
+                    <option value=''>---Select from the following---</option>
+                    <option value='Junior Web Developer'>Junior Developer</option>
+                    <option value='Senior Web Developer'>Senior Developer</option>
+                    <option value='Lead Web Developer'>Lead Developer</option>
+                </select>
+            </label>
+
+            <div className='submit'>
+                <button>submit</button>
+            </div>
+        </div>
+    </form>
+    )
+
+}
