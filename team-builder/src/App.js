6bd223b2eaa1f215ae4f5bd9ac61a8888bea7d2f
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-01-26 15:48:14 -0800

    mvp complete

---
+++ team-builder/src/App.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import './App.css';
+import Form from "./Form"
+import Member from "./Member"
+
+
+
+const initialMemberList = {
+  name: "",
+  email: "",
+  role: "",
+}
+
+
+function App() {
+
+const [member, setMember] =useState([])
+const [memberList, setMemberList] = useState(initialMemberList)
+
+const updateForm = (inputName, inputValue) => {
+  setMemberList({
+    ...memberList,
+    [inputName]: inputValue,
+  })
+}
+
+const submitForm = () => {
+  const newMember = {
+    name: memberList.name.trim(),
+    email: memberList.email.trim(),
+    role: memberList.role,
+  }
+  
+  setMember([newMember, ...member])
+  setMemberList(initialMemberList)
+}
+
+  return (
+    <div className="container">
+      <h1>Form Project</h1>
+      <Form 
+        values={memberList}
+        update={updateForm}
+        submit={submitForm}
+      />
+      {
+        member.map(mem => {
+          return<Member details={mem}/>
+        })
+      }
+      
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/Member.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Member(props) {
+
+    const { details } = props 
+
+    if (!details) {
+        return <h3>Working on fetching details...</h3>
+      }
+
+    return(
+        <div className="friend container">
+            <h2>{details.name}</h2>
+            <p>Email: {details.email}</p>
+            <p>Role: {details.role}</p>
+        </div>
+    )
+}
