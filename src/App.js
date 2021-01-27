aca7fa4ad227b2156f5f4a2913656537297bed45
hdm007 <harry.mardirossian13@gmail.com>
2021-01-26 23:07:50 -0500

    Added useState for team mambers, and default values

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 import React, { useState } from "react"
 
 function App() {
+
+  const [users, setUsers] = useState(
+    [
+      {"name": "Harry", "role": "point"}, {"name": "Reza", "role": "strategy"}, {"name": "Mike", "role": "support"}, {"name": "Kenta", "role":"support"}
+    ])
+    
   return (
     <div className="App">
     </div>
