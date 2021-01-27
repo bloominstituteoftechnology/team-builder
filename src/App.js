f3d73fa2c8d5f98b22b455bf2f24565b79e8eacd
hdm007 <harry.mardirossian13@gmail.com>
2021-01-26 23:26:32 -0500

    team renders from state

--- src/App.js
+++ src/App.js
@@ -7,9 +7,20 @@ function App() {
     [
       {"name": "Harry", "role": "point"}, {"name": "Reza", "role": "strategy"}, {"name": "Mike", "role": "support"}, {"name": "Kenta", "role":"support"}
     ])
-    
+
   return (
     <div className="App">
+      <div>
+        <h3> Team Members </h3>
+        {users.map((user) => {
+          return (
+            <div>
+              <h5>{user.name}</h5>
+              <h6>{user.role}</h6>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
