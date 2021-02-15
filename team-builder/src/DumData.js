94454d6121402c5107cb8661550557c55d83a9b5
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-02-15 00:21:22 -0500

    mvp

---
+++ team-builder/src/DumData.js
@@ -0,0 +1,18 @@
+const TeamData = [
+    {
+      username: 'Black Panther',
+      role: 'Full Stack Web Dev',
+      email: 'blackcat@gmail'
+    },
+    {
+      username: 'Batman',
+      role: 'Backend Web Dev',
+      email: 'gothamknight@gmail'
+    },
+    {
+      username: 'Michael Jordan',
+      role: 'Frontend Web Dev',
+      email: 'baseball@gmail'
+    },
+  ];
+ export default TeamData 
