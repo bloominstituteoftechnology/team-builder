4367f3f90f459f7970f880050a4f55b608ede956
andrewroti <andrewroti@gmail.com>
2021-02-15 05:05:12 -0500

    added onChange handlers

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -2,26 +2,32 @@ import React, { useState } from 'react';
 import './Form.css';
 
 function Form(){
-    const [name, setName] = useState('');
+    const [form, setForm] = useState({})
+    
     
     const handleSubmit = event =>{
        event.preventDefault()
         console.log('Clicked!')
     }
+
+    const handleChanges = event =>{
+        setForm({...form, [event.target.name]: event.target.value});
+    }
+   
     return (
         <div className="form-container" >
             <form onSubmit={handleSubmit}>
                 <label>
                     Name: 
-                    <input placeholder='First and Last Name' type='text' id='name'/><br/>
+                    <input placeholder='First and Last Name' type='text'  name='name' onChange={handleChanges}/><br/>
                 </label>
                 <label>
                     Email:
-                    <input placeholder='Email Address' type='email' id='email'/><br/>
+                    <input placeholder='Email Address' type='email' name='email' onChange={handleChanges}/><br/>
                 </label>
                 <label>
                     Role:
-                    <select>
+                    <select name='role' onChange={handleChanges}>
                         <option>---Select A Role---</option>
                         <option value="student">Student</option>
                         <option value="instructor">Instructor</option>
