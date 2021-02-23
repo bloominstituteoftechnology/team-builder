589233e5e3248cdb8193c193069936e535df065e
Weston Woodard <westonwoodard28@gmail.com>
2021-02-23 15:24:39 -0700

    rough draft complete. now to work on axios and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
+import TeamMemberCards from './TeamMemberCards';
 import './App.css';
 
+
+const initialTeamMembers = [{
+  Name: 'Weston Woodard',
+  Email: 'westonwoodard@gmail.com',
+  Role: 'Director',
+},
+{
+  Name: 'Connie Reynolds',
+  Email: 'conniereynolds@gmail.com',
+  Role: 'Frontend Engineer',
+},
+{
+  Name: 'Jonathan Ryan',
+  Email: 'jonathanryan@gmail.com',
+  Role: 'Backend Engineer',
+}]
+
+const initialFormValues = {
+  Name: '',
+  Email: '',
+  Role: '',
+}
+
 function App() {
+  
+  const [teamMembers, setTeamMembers] = useState(initialTeamMembers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({
+      ...formValues,
+      [inputName]: inputValue
+    })
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      Name: formValues.Name.trim(),
+      Email: formValues.Email.trim(),
+      Role: formValues.Role.trim(),
+    }
+
+    // check web guided lesson app.js starting line 39 to finish this
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+      values={formValues}
+      update={updateForm}
+      submit={submitForm}
+      />
+
+      {teamMembers.map(teamMember => {
+        return (
+          <TeamMemberCards key={teamMember.id} teamMember={teamMember} />
+        )
+      })}
     </div>
   );
 }
