2f03c444f1a89cccc55974990046f004c05c2669
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-23 22:15:51 +0000

    Form component developed, tested and working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
 import sampleData from './data/sampleData';
+import Form from './components/Form';
 import './App.css';
 
 function App() {
   const [teamMembers, setTeamMembers] = useState(sampleData);
   
+  const addNewMember = newMember => {
+    setTeamMembers([...teamMembers, newMember]);
+  }
+
   return (
     <div className="App">
       <h1>Team Members</h1>
@@ -19,6 +24,7 @@ function App() {
           );
         })
       }
+      <Form addNewMember={addNewMember}/>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,77 @@
 import React, { useState } from 'react';
 
-function Form() {
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
 
+function Form(props) {
+  const { addNewMember } = props;
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const onChange = evt => {
+    const { name, value } = evt.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+    const newMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (newMember.name && newMember.email && newMember.role) {
+      addNewMember(newMember);
+      setFormValues(initialFormValues);
+    }
+    else {
+      console.log(`newMember missing field: ${newMember.name} ${newMember.email} ${newMember.role}`);
+    }
+  }
+
+  return (
+    <form className='form container' onSubmit={onSubmit}>
+      <h2>Add New Team Member</h2>
+      <label>Name
+        <input 
+          name='name'
+          type='text'
+          value={formValues.name}
+          onChange={onChange}
+          placeholder='Firstname Lastname'
+          required
+        />
+      </label>
+      <label>Email
+        <input 
+          name='email'
+          type='email'
+          value={formValues.email}
+          onChange={onChange}
+          placeholder='firstlast@company.com'
+          required
+        />
+      </label>
+      <label>Role
+        <select name='role' value={formValues.role} onChange={onChange} required>
+          <option value='' selected disabled>--- Select role ---</option>
+          <option value='Android Developer'>Android Developer</option>
+          <option value='Back-end Engineer'>Back-end Engineer</option>
+          <option value='Data Scientist'>Data Scientist</option>
+          <option value='Front-end Engineer'>Front-end Engineer</option>
+          <option value='Full-stack Engineer'>Full-stack Engineer</option>
+          <option value='iOS Developer'>iOS Developer</option>
+          <option value='UX Designer'>UX Designer</option>
+        </select>
+      </label>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default Form;
