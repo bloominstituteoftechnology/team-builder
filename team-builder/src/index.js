39bfd38fa928b4bbaf37441f216f35d7411c2ce9
Mitchell Walker <eagleye1144@gmail.com>
2021-02-23 16:21:22 -0700

    moved over to Form.js and added some handlers

--- team-builder/src/index.js
+++ team-builder/src/index.js
@@ -11,44 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const teamMembers = [
-  {name: 'Mitch'},
-  {name: 'Alex'},
-  {name: 'Sean'}
-]
 
-const initialFormValues = {
-  name: '',
-}
-
-function SimpleForm() {
-  const {team, setTeam} = useState(teamMembers);
-  const {newName, setNewName} = useState(initialFormValues);
-
-const change = event =>{
-
-
-}
-return (
-  <div className = "container">
-    <h1>Simple Form</h1>
-      {teamMembers.map((nme, idx) => {
-        return (
-        <div key = {idx}>
-          Team Members are {nme.name}
-          </div>
-
-      )})
-      }
-      <form>
-        <input name='name' type='text' onChange={change} value ={newName.name}/> 
-      </form>
-  </div>
-
-
-)
-
-}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
