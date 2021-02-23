fea315ba902782e2f41c2633f8bf6b0a21388b19
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-23 22:25:33 +0000

    grouped Form JSX, MVP complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -38,40 +38,44 @@ function Form(props) {
 
   return (
     <form className='form container' onSubmit={onSubmit}>
-      <h2>Add New Team Member</h2>
-      <label>Name
-        <input 
-          name='name'
-          type='text'
-          value={formValues.name}
-          onChange={onChange}
-          placeholder='Firstname Lastname'
-          required
-        />
-      </label>
-      <label>Email
-        <input 
-          name='email'
-          type='email'
-          value={formValues.email}
-          onChange={onChange}
-          placeholder='firstlast@company.com'
-          required
-        />
-      </label>
-      <label>Role
-        <select name='role' value={formValues.role} onChange={onChange} required>
-          <option value='' selected disabled>--- Select role ---</option>
-          <option value='Android Developer'>Android Developer</option>
-          <option value='Back-end Engineer'>Back-end Engineer</option>
-          <option value='Data Scientist'>Data Scientist</option>
-          <option value='Front-end Engineer'>Front-end Engineer</option>
-          <option value='Full-stack Engineer'>Full-stack Engineer</option>
-          <option value='iOS Developer'>iOS Developer</option>
-          <option value='UX Designer'>UX Designer</option>
-        </select>
-      </label>
-      <button>Submit</button>
+      <div className='form-inputs'>
+        <h2>Add New Team Member</h2>
+        <label>Name
+          <input 
+            name='name'
+            type='text'
+            value={formValues.name}
+            onChange={onChange}
+            placeholder='Firstname Lastname'
+            required
+          />
+        </label>
+        <label>Email
+          <input 
+            name='email'
+            type='email'
+            value={formValues.email}
+            onChange={onChange}
+            placeholder='firstlast@company.com'
+            required
+          />
+        </label>
+        <label>Role
+          <select name='role' value={formValues.role} onChange={onChange} required>
+            <option value='' selected disabled>--- Select role ---</option>
+            <option value='Android Developer'>Android Developer</option>
+            <option value='Back-end Engineer'>Back-end Engineer</option>
+            <option value='Data Scientist'>Data Scientist</option>
+            <option value='Front-end Engineer'>Front-end Engineer</option>
+            <option value='Full-stack Engineer'>Full-stack Engineer</option>
+            <option value='iOS Developer'>iOS Developer</option>
+            <option value='UX Designer'>UX Designer</option>
+          </select>
+        </label>
+      </div>
+      <div className='submit'>
+        <button>Submit</button>
+      </div>
     </form>
   );
 }
