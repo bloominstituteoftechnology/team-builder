ebe97651226bb672f29d2e73130a988d9a9e6ba4
Marina Monsivais <minamonsi@me.com>
2021-03-14 19:04:19 -0500

    Installed axios & added submit handlers

--- team-builder/src/components/MemberForm.js
+++ team-builder/src/components/MemberForm.js
@@ -1,11 +1,24 @@
-import React,{useState} from "react"
-
+import React,{useState} from "react";
+import axios from "axios";
 
 export function App() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [role, setRole] = useState('');
   const [status, setStatus] = useState(false)
+  const onSubmit = event => {
+    event.preventDefault();
+    axios.post('/', {username, email, role, status});
+  }
+
+  const onUsernameChange = event => setUsername(event.target.value)
+  const onEmailChange = event => setEmail(event.target.value)
+  const onRollChange = event=> setRole(event.target.value)
+  const onStatusChange = event => setStatus(event.target.checked)
+  
+
+  
+  
 
     return (
       <div className="container"> 
@@ -14,11 +27,11 @@ export function App() {
       <h2>Email: {email || ''}</h2>
       <h2>Role: {role || ''}</h2>
       <h2>Status: {status ? 'active' : 'inactive'}</h2>
-      <form>
+
+      <form onSubmit={onSubmit}>
+
         <label htmlFor='usernameInput'>Username: </label> 
-        <input onChange={event => {
-          setUsername(event.target.value)
-        }}
+        <input onChange={onUsernameChange}
               type="text"
               id="usernameInput"
               name="username"
@@ -28,9 +41,7 @@ export function App() {
         /><br/>
 
         <label htmlFor='emailInput'>Email: </label>
-        <input onChange={event =>{
-          setEmail(event.target.value)
-        }}
+        <input onChange={onEmailChange}
              type="text"
              id="emailInput"
              name="email"
@@ -40,9 +51,7 @@ export function App() {
           /><br/>
   
         <label htmlFor="roleSelect">Role:</label> 
-          <select onChange={event=> {
-            setRole(event.target.value)
-          }}
+          <select onChange={onRollChange}
           id="roleSelect" 
           name="role">
             <option value='Student'>Student</option>
@@ -51,11 +60,8 @@ export function App() {
             <option value='Admin'>Admin</option>
           </select><br/>
           
-
         <label htmlFor="status">Active member?</label>
-          <input onChange={event => {
-            setStatus(event.target.checked)
-          }}
+          <input onChange={onStatusChange}
           type="checkbox"
           id="status"
           value="yes"
