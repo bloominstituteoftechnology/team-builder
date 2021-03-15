34cc8c25b9dbfd9176fab0185858f95c34e17b87
Kilgharrahcalls <69174493+Kilgharrahcalls@users.noreply.github.com>
2021-03-15 11:57:10 -0400

    fixed

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "./Form.js";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 export default function App() {
+
+  // Import & lift state from form to add to friend list.
+  // const [friend, setFriend] = useState(friends);
+
+
+
+
   return (
     <div className="App container d-flex justify-content-center">
       <div className="form-card d-flex justify-content-center">
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,96 +1,80 @@
-import React, { useState } from "react";
-import friends from "./friends.js";
-import { v4 as uuidv4 } from "uuid";
-import axios from "axios";
+import React, { useState } from "react";
+import friends from "./friends.js";
 
-const initialFormsValues = {
-  firstName: "",
-  nationality: "",
-  status: "",
+const initialFormsValues = {
+  firstName: "",
+  nationality: "",
+  status: "",
 };
+export default function Form() {
+  const [friend, setFriend] = useState(friends);
+  const [formValues, setFormValues] = useState(initialFormsValues);
 
-export default function Form() {
-  const [friend, setFriend] = useState(friends);
-  const [formValues, setFormValues] = useState(initialFormsValues);
+    const submit = (e) => {
+      //prevent submit
+    e.preventDefault();
+        //create new object to add to friends array
+    const toBeUpdated = {
+        firstName: formValues.firstName,
+        nationality: formValues.nationality,
+        status: formValues.status
+        };
+        //spread the current state and then add {toBeUpdated} to the end of the list
+    setFriend([...friend, toBeUpdated]);
+        //reset to initial values
+    setFormValues(initialFormsValues); 
+  }
 
-  const postNewFriend = (newFriend) => {
-    axios.post("http://regres.in/api/users", newFriend)
-    .then((res) => {
-      setFriend([res.data,...friend]);
-      console.log("Api success", res.data);
-      setFormValues(initialFormsValues)
-    })
-    .catch((err)=>{
-      console.log("error", err)
-    })
-  }
-  const submit = (e) => {
-    e.preventDefault();
-  
 
-    const toBeUpdated = { ...friend };
-    toBeUpdated.firstName = formValues.firstName;
-    toBeUpdated.nationality = formValues.nationality;
-    toBeUpdated.status = formValues.status;
-    setFriend(toBeUpdated);
-    setFormValues(initialFormsValues);
-  }
-  const Change = (e) => {
-    const { name, value } = e.target;
-    setFormValues((previousFormValues) => ({
-      ...previousFormValues,
-      [name]: value
-    }));
-  };
+  const change = (e) => {
+    const { name, value } = e.target;
+    setFormValues((previousFormValues) => ({
+      ...previousFormValues,
+      [name]: value
+    }));
+  };
+//   const newFriend = {
+//     firstName: formValues.firstName,
+//     nationality: formValues.nationality,
+//     status: formValues.status,
+//   }
 
-  const newFriend = {
-    id: uuidv4(),
-    firstName: formValues.firstName,
-    nationality: formValues.nationality,
-    status: formValues.status,
-  };
-  postNewFriend(newFriend)
-  setFormValues(initialFormsValues)
-  
-  // setFriend(friend.concat(newFriend))
-  // setFormValues(initialFormsValues)
-
-  return (
-    <div>
-      <form onSubmit={submit}>
-        <input
-          onChange={Change}
-          value={formValues.firstName}
-          placeholder="First Name"
-          id="firstNameInput"
-          name="firstName"
-          type="text"
-        />
-        <input
-          onChange={Change}
-          value={formValues.nationality}
-          placeholder="nationality"
-          id="nationalityInput"
-          name="nationality"
-          type="text"
-        />
-        <input
-          onChange={Change}
-          value={formValues.status}
-          placeholder="status"
-          id="statusInput"
-          name="status"
-          type="text"
-        />
-        <button type="submit">submit</button>
-      </form>
-      {friend.map((friends, idx) => (
-        <div style={{ border: ".15rem solid black" }}key={idx}>
-          <h3>{friends.firstName}</h3>
-          <p>{friends.nationality}</p>
-          <p>{friends.status}</p>
-        </div>
-      ))}
-    </div>
-  );
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <input
+          onChange={change}
+          value={formValues.firstName}
+          placeholder="First Name"
+          id="firstNameInput"
+          name="firstName"
+          type="text"
+        />
+        <input
+          onChange={change}
+          value={formValues.nationality}
+          placeholder="nationality"
+          id="nationalityInput"
+          name="nationality"
+          type="text"
+        />
+        <input
+          onChange={change}
+          value={formValues.status}
+          placeholder="status"
+          id="statusInput"
+          name="status"
+          type="text"
+        />
+        <button type="submit">submit</button>
+      </form>
+      {(friend && friend.map((friends, idx) => (
+        <div style={{ border: ".15rem solid black" }}key={idx}>
+          <h3>{friends.firstName}</h3>
+          <p>{friends.nationality}</p>
+          <p>{friends.status}</p>
+        </div>
+      )))}
+    </div>
+  );
 }
