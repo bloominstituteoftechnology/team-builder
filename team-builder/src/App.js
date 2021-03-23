c61c8779b681e7c09a469d49f80e857a4d18f1f3
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-23 19:05:11 -0400

    Added input fields, rendered initial team array

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,36 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import TeamForm from './Form'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TeamForm />
+      
     </div>
   );
-}
-
-const initialFormValue = {
-  teamMemberName: '',
-  teamMemberEmail: '',
-  teamMemberRole: '',
-}
-
-function Form() {
-  const [teamMembers, setTeamMembers] = useState([])
 }
 
 export default App;
--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -1,5 +1,21 @@
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import {ReactDOM, render} from 'react-dom';
+
+
+const codeTeam = [
+    {
+        teamMemberName: 'William Dye' , teamMemberEmail: 'dye@dye.com',
+        teamMemberRole: 'Director',
+    },
+    {
+        teamMemberName: 'Daniel Herrera', teamMemberEmail: 'herrera@herrera.com',
+        teamMemberRole: 'Head Chef',
+    },
+    {
+        teamMemberName: 'Chloe Dye', teamMemberEmail: 'steeg@steeg.com' ,
+        teamMemberRole: 'Professional Steeg',
+    }
+]
 
 const initialFormValue = {
     teamMemberName: '',
@@ -7,6 +23,43 @@ const initialFormValue = {
     teamMemberRole: '',
   }
   
-  function Form() {
-    const [teamMembers, setTeamMembers] = useState([])
+  function TeamForm() {
+    const [teamMembers, setTeamMembers] = useState(codeTeam)
+
+    return (
+      <div>
+        <h1>Team Builder</h1>
+        <form>
+          <label>
+            New Member Name:
+            <input name='memberName' type='text'/>
+          </label>
+          <br></br>
+          <label>
+            New Member Email:
+            <input name='memberEmail' type='text'/>
+          </label>
+          <br></br>
+          <label>
+            New Member Role:
+            <input name='memberRole' type='text' />
+          </label>
+        </form>
+        <h2>Meet the Team:</h2>
+        {codeTeam.map((member, idx) => {
+            return (
+                <div key={idx}>
+                  {member.teamMemberName}, {member.teamMemberRole}, {member.teamMemberEmail}
+                </div>
+            
+            )
+        })}
+      </div>
+    )
+      
+
   }
+
+
+
+  export default TeamForm
