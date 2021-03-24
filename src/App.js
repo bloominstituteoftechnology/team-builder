774b538b99057873b33100e49b74bf3028992923
alextran0899 <alextran@MacBook-Air.local>
2021-03-23 22:52:32 -0500

    alex-tran

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import React, { useState } from "react"
+
 
 function App() {
+  const setDefault= {
+    name: '',
+    email: '',
+    role: '',
+}
+const friendsList = [ 
+  { 
+    name: 'alex',
+    email: 'alextran0899@gmail.com',
+    role: 'Lambda Student'
+  },
+  {
+    name: 'face',
+    email: 'legit@noemail.com',
+    role: 'Student'
+  }
+]
+
+const [newFriend, setNewFriend] = useState(setDefault)
+const [list,setList] = useState(friendsList);
+ 
+const submitHandeler = evt =>{ 
+  setList([...list, newFriend])
+  evt.preventDefault();
+}
+const newFriendHandler = evt =>{
+  setNewFriend({...newFriend, [evt.target.name]: evt.target.value })
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Form list={list} update={newFriendHandler} submit={submitHandeler}/>    
+    {list.map((friend) => <li >Name: {friend.name} Email: {friend.email} Role: {friend.role}</li>)}
     </div>
+
   );
 }
 
---
+++ src/Form.js
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+
+ function Form(props) {
+
+ const {submit, update, list} = props;
+
+     return (
+         <div>
+             <form onSubmit={submit}>
+                 <label>Name
+                 <input
+                     onChange={update}
+                     maxLength='20'
+                     placeholder='Name'
+                     name='name'
+                     type='text'
+                 />
+                 </label>
+                 <label>Email
+                 <input
+                     onChange={update}
+                     maxLength='30'
+                     placeholder='Email'
+                     name='email'
+                     type='text'
+                 />
+                 </label>
+                 <label>Role
+                 <input
+                     onChange={update}
+                     maxLength='10'
+                     placeholder='Role'
+                     name='role'
+                     type='text'
+                 />
+                 </label>
+                 <input 
+                     type='submit'
+                     value='Submit'
+                 />
+             </form>
+         </div>
+
+
+     )
+ }
+
+ export default Form; 
