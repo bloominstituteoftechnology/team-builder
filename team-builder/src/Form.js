c0f17f8c36a2bf46b15e7a4c4cbf20d49f742b40
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-03-23 20:59:07 -0400

    Added labels

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -25,28 +25,46 @@ const initialFormValue = {
   
   function TeamForm() {
     const [teamMembers, setTeamMembers] = useState(codeTeam)
+    const [formValues, setFormValues] = useState(initialFormValue)
+
+    const change = evt => {
+        const { name, value } = evt.target
+        setFormValues({...formValues, [name]: value})
+    }
+
+    const submit = evt => {
+        evt.preventDefault()
+        const newTeamMember = {
+            teamMemberName: formValues.teamMemberName.trim(),
+            teamMemberEmail: formValues.teamMemberEmail.trim(),
+            teamMemberRole: formValues.teamMemberName.trim(),
+        }
+    }
 
     return (
       <div>
         <h1>Team Builder</h1>
-        <form>
+        <form onSubmit={submit}>
           <label>
             New Member Name:
-            <input name='memberName' type='text'/>
+            <input name='memberName' type='text' onChange={change} value={formValues.teamMemberName}/>
           </label>
           <br></br>
           <label>
             New Member Email:
-            <input name='memberEmail' type='text'/>
+            <input name='memberEmail' type='text' onChange={change} value={formValues.teamMemberEmail}/>
           </label>
           <br></br>
           <label>
             New Member Role:
-            <input name='memberRole' type='text' />
+            <input name='memberRole' type='text' onChange={change} value={formValues.teamMemberRole} />
           </label>
+          <br></br>
+          <br></br>
+            <button>Submit</button>
         </form>
         <h2>Meet the Team:</h2>
-        {codeTeam.map((member, idx) => {
+        {teamMembers.map((member, idx) => {
             return (
                 <div key={idx}>
                   {member.teamMemberName}, {member.teamMemberRole}, {member.teamMemberEmail}
