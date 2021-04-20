39743ff42569bb4b205ed37796ec04641e092562
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-04-20 16:48:54 -0400

    imported useState and set up state to keep my team members list

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState([
+    {
+      id: 0,
+      name: '',
+      role: '',
+      email: ''
+    }
+  ]);
+
+  console.log(teamMembers);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
