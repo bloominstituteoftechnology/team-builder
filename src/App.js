b0ff6e1aca448ad362068cd1a343e54e3b6a5ae6
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-20 23:37:50 -0600

    update to app and form js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState }  from 'react';
 import ReactDOM from 'react-dom';
+import TeamMember from './components/Form';
 
 const defaultMember = [
   {
@@ -22,6 +23,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Members of Team</h1>
+      < TeamMember teamMember={teamMember}/>
     </div>
   );
 }
