a954203f3b7b8a6c628d680ce9ff56185fbddc18
Peter Benjamin Mhina <peterbenjaminmhina@Peters-MacBook-Pro.local>
2021-04-20 20:08:34 -0700

    DONE

---
+++ team-builder/src/FormMembers.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const TeamMembers = ({ teamMembers }) => {
+
+    return (
+        <div>
+            {teamMembers.map((member, index) => {
+            return (
+                <div key={index}>
+                    <h2>Name: {member.name}</h2>
+                    <p>Email: {member.email} </p>
+                    <p>Role: </p>
+                </div> 
+            )   
+            })}
+        </div>
+    )
+}
+
+export default FormMembers;
