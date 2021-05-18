babf87c19721c86bcc160378d4f71cea5ba57df6
ferengitooth <ehitsamill@gmail.com>
2021-05-18 15:26:26 -0700

    set up form.js and injected it into app

---
+++ team-builder/src/App.js
@@ -0,0 +1,14 @@
+  import './App.css';
+import React, {useState, useEffect} from 'react'
+import TeamCard from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <TeamCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TeamCard = props => {
+    return (
+        <div>
+            <h3>Name</h3>
+            <h3>Role</h3>
+            <h3>Email</h3>
+        </div>
+    )
+}
+
+export default TeamCard
