aa67f3d9265244293c63a6afe2a53ac6f72e1c9e
John Vehmeyer <j.vehmeyer@gmail.com>
2021-05-18 15:32:44 -0500

    successfully rendered form to webpage

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
+import TeamForm from './TeamForm'
 
 // initial state
 const initialFormValues = {
@@ -12,7 +13,20 @@ function App() {
   const[teammate, setTeammate] = useState([])
   const [formValues, setFormValues] = useState(initialFormValues)
 
-  
+  // form state updater function
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue })
+  }
+
+  // submit form function
+  const submitForm = () => {
+    // new teammate object
+    const newTeammate = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+  }
 
 
   return (
@@ -24,7 +38,11 @@ function App() {
         add props to inject in child forms
         /> */}
 
-
+        <TeamForm
+          update={updateForm}
+          submit={submitForm}
+          values={formValues}
+        />
       </header>
     </div>
   );
