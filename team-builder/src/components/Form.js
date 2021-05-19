c33ebd4be8d1afd12cb62523f0de2e83348c2908
ferengitooth <ehitsamill@gmail.com>
2021-05-18 19:46:25 -0700

    fleshed out the form. added labels and inputs for name,roll, and email.

--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,10 +1,28 @@
   import React, {useState, useEffect} from 'react'
 
 const Form = props => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        role: '',
+        email: '',
+    })
+
     return (
         <div>
            <form>
-            <input />
+            <label>
+                Name
+                <input />
+            </label>
+            <label>
+                Roll
+                <input />
+            </label>
+            <label>
+                Email
+                <input />
+            </label>
            </form>
         </div>
     )
