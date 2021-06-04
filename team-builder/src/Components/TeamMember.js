e04ec1fd44546657e75447312870c52422035cd0
Greg Brewton <gbrewton1980@gmail.com>
2021-06-04 13:06:59 -0600

    Styles app with Material UI, Icons

--- team-builder/src/Components/TeamMember.js
+++ team-builder/src/Components/TeamMember.js
@@ -1,12 +1,32 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import Send from "@material-ui/icons/Send";
 
 const TeamMember = (props) => {
   const { name, email, role } = props;
   return (
     <div className="team_member">
-      <h2>{name}</h2>
-      <p>Role: {role}</p>
-      <p>Email: {email}</p>
+      <Card raised>
+        <CardContent>
+          <Typography variant="h4" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="subtitle1">{role}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" endIcon={<Send />}>
+            <a style={{ textDecoration: "none", color: "inherit" }} href={`emailto:${email}`}>
+              Email {name}
+            </a>
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 };
