328c1bab9a4397b6fafd5c652c5455b1afa8f814
vida <ofo28@aol.com>
2021-06-04 23:57:12 -0400

    working on the Styling of the components

---
+++ src/components/Form.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+
+
+export default function TeamForm(props) {
+    const { values, update, submit } = props
+
+    const onChange = evt => {
+      
+    const { name, value } = evt.target
+      
+      update(name, value)
+    }
+  
+    const onSubmit = evt => {
+      
+      evt.preventDefault()
+      
+      submit()
+    }
+
+
+return (
+<form className='form container' onSubmit={onSubmit}>
+      <div className='form-group inputs'>
+      <div>  
+        <label>Team Member Name
+          
+              <input 
+                type="text" name="name"
+                value={values.name}
+                placeholder="Type a Member Name"
+                onChange={onChange}
+                
+              />
+        </label>
+
+        </div>
+        <div>
+        <label>Email
+         
+          <input 
+            type="email"
+            name="email"
+            value={values.email}
+            placeholder="Type a email"
+            onChange={onChange}
+          />
+        </label>
+    </div>    
+    <div>
+        <label>Role
+          
+          <select name="role"  value={values.role} onChange={onChange}>
+            <option value="">select role!</option>
+            <option value="Backend engineer">Backend engineer</option>
+            <option value="Front engineer">Front engineer</option>
+            <option value="Data Analyst">Data Engineer</option>
+            <option value="Full Stack">Full Stack</option>
+          </select>
+          
+        </label>
+    </div>
+
+        <div className='submit'>
+          <button>submit</button>
+        </div>
+      </div>
+</form>
+)
+}
