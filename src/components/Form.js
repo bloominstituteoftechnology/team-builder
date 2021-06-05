0a8dbc7756fe00f456e975f8af9f0620469c6fdf
vida <ofo28@aol.com>
2021-06-05 01:48:45 -0400

    working on the Styling of the components 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,33 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
 
+  color: ${(props) => props.besty ? props.theme.colors.dangerColor : props.theme.colors.primaryColor};
+  font-weight: ${props => props.theme.text.bold};
+  &:hover {
+    color: ${props => props.theme.colors.secondaryColor};
+  }
+  button {
+     background-color: ${props => props.theme.colors.vsCodeBlue};
+     &:hover{
+    animation: 1s ${fadeIn} ease-out;
+     transform: scale(1.1);
+     background-color: ${props => props.theme.colors.paleSky}
+     }
+  }
+`
 
 export default function TeamForm(props) {
     const { values, update, submit } = props
@@ -22,7 +49,7 @@ export default function TeamForm(props) {
 
 return (
 <form className='form container' onSubmit={onSubmit}>
-      <div className='form-group inputs'>
+      <StyledFriend className='form-group inputs'>
       <div>  
         <label>Team Member Name
           
@@ -65,7 +92,7 @@ return (
         <div className='submit'>
           <button>submit</button>
         </div>
-      </div>
+      </StyledFriend>
 </form>
 )
 }
---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+// theme object here
+
+const themeObject = {
+    colors: {
+       primaryColor: 'dodgerblue',
+       secondaryColor: 'crimson',
+       dangerColor: 'orange',
+       vsCodeBlue: '#007AAF',
+       paleSky: 'cyan'
+    },
+    text: {
+        bold: 'bold',
+    }
+}
+
+export default themeObject;
