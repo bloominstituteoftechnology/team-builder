6ab76a5e6992adc630a0d605ee79d6960ab1e3c8
Zachary Gooding <zac.gooding@gmail.com>
2021-06-15 23:08:47 -0400

    added name input

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React, { useState }from 'react'
+
+const Form = () => {
+    const [teamMembers, setTeamMembers] = useState('')
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Form
