d7a391ae7946e6d870c6bdf65b7b02503af6be9e
tamara-taylor <ttaylor8522@gmail.com>
2021-07-10 12:52:40 -0400

    finished mvp

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Form (props) {
+    const { update, submit, val } = props;
+
+    const onChange = event => {
+        const { name, value } = event.target
+        update (name, value)
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+        submit()
+    }
+
+    return (
+        <form className='container' onSubmit={onSubmit}>
+            <div className='form-group inputs'>
+                <label>Name
+                    <input type='text' value={val.name} onChange={onChange} name='name' placeholder='Name' maxLength='15' />
+                </label>
+                <label>Email
+                    <input type='text' value={val.email} onChange={onChange} name='email' placeholder='Email' maxLength='50' />
+                </label>
+                <label>Role
+                    <select value={val.role} name='role' onChange={onChange}>
+                        <option value=''>Select</option>
+                        <option value='CNA'>CNA</option>
+                        <option value='Designer'>Designer</option>
+                        <option value='Software Engineer'>Software Engineer</option>
+                        <option value='Full Stack Web Developer'>Full Stack Web Developer</option>
+                        <option value='Construction'>Construction</option>
+                    </select>
+                </label>
+                <div className='submit'>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </form>
+    )
+}
