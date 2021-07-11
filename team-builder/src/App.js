6234c71b31e8fc388b63e10923d30ccfb5002202
jarghiskhan <jared.l.wyatt@outlook.com>
2021-07-10 23:03:49 -0400

    Initial Setup

---
+++ team-builder/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <Form />
+  );
+}
+
+export default App;
---
+++ team-builder/src/Form.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Team Member Name"></input>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
