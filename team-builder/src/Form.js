2738dbacc5fe0c4ea6a3c1e412e51e8966d1579a
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-11 22:39:30 -0500

    forgot to add button

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -9,16 +9,17 @@ function Form() {
 		<form className="col">
 			<div className="row">
 				<label htmlFor="name" className="form-label">Name:</label>
-				<input type="text" className="form-control-lg" placeholder="enter your name" />
+				<input type="text" className="form-control-lg" name="name" placeholder="enter your name" />
 			</div>
 			<div className="row">
 				<label htmlFor="email" className="form-label">Email:</label>
-				<input type="email" className="form-control-lg" placeholder="enter your email" />
+				<input type="email" className="form-control-lg" name="email" placeholder="enter your email" />
 			</div>
 			<div className="row">
 				<label htmlFor="email" className="form-label">Role:</label>
-				<input type="text" className="form-control-lg" placeholder="what is your role?" />
+				<input type="text" className="form-control-lg" name="role" placeholder="what is your role?" />
 			</div>
+			<button type="button" className="btn btn-sm btn-primary">Submit</button>
 			
 		</form>
 	)
