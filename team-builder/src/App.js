f4dbfda8b881b2b0a9494498c92c074484a68cee
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-11 23:55:54 -0500

    setting up member.js to display members of my team

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,14 +1,23 @@
 import {useState} from 'react'
 import './App.css';
 import Form from './Form'
+import Member from './Member'
 
 function App() {
 
+  const [member, setMember] = useState([{
+    name:"Steve",
+    email: "purefallen11@gmail.com",
+    role: "Software Developer"
+  }])
+
+  
  
   return (
     <div className="App">
       <header className="App-header">
         <Form />
+        <Member member={member} />
       </header>
     </div>
   );
---
+++ team-builder/src/Member.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Member({member}) {
+	return (
+		<div></div>
+	)
+}
+
+export default Member
