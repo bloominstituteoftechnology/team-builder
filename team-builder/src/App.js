57fe760ed14f2d5096c42eae9ebefddee9dc8e22
rdperry031 <rdperry031@gmail.com>
2021-07-20 15:56:27 -0500

    started building form page

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
+const initialValues = {
+  username: '',
+  email: '',
+  role: '',
+}
+
 
 function App() {
+  const [teamMember, setMember] = useState({ })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FOORMS</h1>
     </div>
   );
 }
---
+++ team-builder/src/Form..js
@@ -0,0 +1,15 @@
+import React from 'react'
+export default function Form(props) {
+    const { values, update, submit }
+    const onChange = evt => {
+
+    }
+    const onSubmit = evt => {
+
+    }
+    return (
+        <form className= 'form-container' onSubmit={onSubmit}>
+            
+        </form> 
+    )
+}
