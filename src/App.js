d023f97c06ff2fe8dfd5c7d284b30cc561bad682
Klove Adams <kalebsxott@gmail.com>
2021-07-20 15:38:52 -0400

    app state set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const initialFormValues = {
+  username: "",
+  email: "",
+  role: "",
+}
+
 function App() {
+  const [team, setTeam] = useState([])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
