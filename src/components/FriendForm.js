fe67478ec5e3573058a822aede26fdae09ab6b59
Rick <rmansfield@realpeoplesratings.com>
2021-07-20 07:28:29 -0500

    U2.3 M2 FriendForm.js set up

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -4,18 +4,11 @@ export default function FriendForm(props) {
   const { values, update, submit } = props
 
   const onChange = evt => {
-
-
     const {name, value} = evt.target
-
-
-   update(name, value)//put a debugger on the parent App.js updateForm and verify working. 
+    update(name, value)
   }
 
   const onSubmit = evt => {
-    // 🔥 STEP 7 - IMPLEMENT the submit handler
-    // a) don't allow the browser to reload!
-    // c) use the `submit` callback coming in through props
     evt.preventDefault()
     submit()
   }
@@ -23,27 +16,18 @@ export default function FriendForm(props) {
   return (
     <form className='form container' onSubmit={onSubmit}>
       <div className='form-group inputs'>
-        {/* ////////// TEXT INPUTS ////////// */}
-        {/* ////////// TEXT INPUTS ////////// */}
-        {/* ////////// TEXT INPUTS ////////// */}
         <label>Username
-          {/* 🔥 STEP 3 - Make an input of type `text` for username.
-              Controlled inputs need `value` and `onChange` props.
-              Inputs render what they're told - their current value comes from app state.
-              At each keystroke, a change handler fires to change app state. 
-              */}
             <input 
             name='username'
             type='text'
             value={values.username}
             onChange={onChange}
             placeholder='type your username'
-            maxLength='30' //ask bakend developer what this should be.       
+            maxLength='30'      
             />
         </label>
 
         <label>Email
-          {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
           <input
           name='email'
             type='email'
@@ -54,17 +38,12 @@ export default function FriendForm(props) {
             />
         </label>
 
-        {/* ////////// DROPDOWN ////////// */}
-        {/* ////////// DROPDOWN ////////// */}
-        {/* ////////// DROPDOWN ////////// */}
-        <label>Role
-          {/* 🔥 STEP 5 - Make dropdown for role. */}
-        
+        <label>Role        
         <select name='role' value={values.role} onChange={onChange}>
           <option value="">--select role--</option>
-          <option value="instructor">Instructor</option>
-          <option value="student">Student</option>
-          <option value="tl">Team Lead</option>
+          <option value="Fackend Engineer">Backend Engineer</option>
+          <option value="Frontend Engineer">Fronend Engineer</option>
+          <option value="Designer">Designer</option>
         </select>
         </label>
 
