ce375ca7710ac035dfda3a7f071fb865061ce8af
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-20 13:03:11 -0400

    Still setting up form.js page

--- team-builder/src/form.js
+++ team-builder/src/form.js
@@ -1,1 +1,44 @@
-export default function Form(props) {}
+export default function Form(props) {
+  const { formValues, update, submit } = props;
+
+  const onChange = (evt) => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+    update(name, value);
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">
+          Name:
+          <input
+            onChange={onChange}
+            type="text"
+            placeholder="Enter Name"
+            name="username"
+            id="name"
+            value={formValues.username}
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input
+            onChange={onChange}
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            id="email"
+            value={formValues.email}
+          />
+        </label>
+        <label htmlFor="role"></label>
+      </form>
+    </div>
+  );
+}
