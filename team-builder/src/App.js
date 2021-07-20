e35cd4dfe151314e4d8e89711f196203939482dc
rdperry031 <rdperry031@gmail.com>
2021-07-20 16:32:45 -0500

    completed Form,probably

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react'
+import Form from './Form.'
 
 const initialValues = {
   username: '',
@@ -12,9 +13,26 @@ const initialValues = {
 function App() {
   const [teamMember, setMember] = useState({ })
   
+  const updateForm = (inputName, inputValue) => {
+    setMember({...teamMember, [inputName]: inputValue})
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      username: teamMember.username.trim(),
+      email: teamMember.email.trim(),
+      role: teamMember.role.trim(),      
+    }
+  }
+  
   return (
     <div className="App">
       <h1>FOORMS</h1>
+      <Form
+      values = {teamMember}
+      update = {updateForm}
+      submit = {submitForm}
+      />
     </div>
   );
 }
