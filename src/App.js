8594db0459ec416664dba3bf82e1977451fba600
Klove Adams <kalebsxott@gmail.com>
2021-07-20 16:22:10 -0400

    added imputs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import Form from ",/Form"
 import './App.css';
 
@@ -7,17 +7,17 @@ const fakeTeam = [
     name: "Jon",
     email: "jon@name.com",
     role: "backend engineer",
-  }
+  },
   {
     name: "Bob",
     email: "bob@name.com",
     role: "frontend engineer",
-  }
+  },
   {
     name: "Tom",
     email: "tom@name.com",
     role: "designer",
-  }
+  },
 ]
 
 const initialFormValues = {
@@ -27,15 +27,18 @@ const initialFormValues = {
 }
 
 function App() {
-  const [team, setTeam] = useState([])
-  
+  const [team, setTeam] = useState([]);
+  useEffect(() =>{
+    return setTeam(fakeTeam)
+  }, []);
+  console.log(team);
   return (
     <div>
       <h1>Team members</h1>
       {/* <Form/> */}
-      <h2>name</h2>
-      <p>email</p>
-      <p>role</p>
+      <h2>{team.name}</h2>
+      <p>Email{team.email}</p>
+      <p>Role{team.role}</p>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,30 @@
 import React from "react";
 
-// export default function Form(props){
-
-// }
+export default function Form(props){
+    <div>
+        <label>Name
+            <input
+                type="text"
+                name="name"
+                onChange={onChange}
+            />
+        </label>
+        <label>Email
+            <input
+            type="email"
+            name="email"
+            onChange={onChange}
+            />
+        </label>
+        <label>Role
+          <select name="role" onChange={onChange}>
+            <option value="">-- Select a Role --</option>
+            <option value="backend engineer">Backend Engineer</option>
+            <option value="frontend engineer">Frontend Engineer</option>
+            <option value="designer">Designer</option>
+            <option value="nocho maker">Nocho Maker</option>
+            <option value="me">Me</option>
+          </select>
+        </label>
+    </div>
+}
