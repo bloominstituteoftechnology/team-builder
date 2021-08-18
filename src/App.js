e084511088ec57723994743a8af6b00e3f7ad284
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-17 22:02:28 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import FormList from "./components/FormList";
 
 function App() {
+  const [members, setMembers] = useState([{}, {}]);
 
-
-
-
-
-
-
-
-
-
-  
   return (
     <div className="App">
-      
+      <h1>Add Team Member</h1>
+
+      <Form members={members} setMembers={setMembers} />
+      <FormList members={members} />
     </div>
   );
 }
