9cc3f0c6366e7f307d7acc234b4b31e284033b90
Alex Hoskins <alexander.hoskins@yahoo.com>
2021-10-12 14:55:19 -0700

    Finished mvp

---
+++ src/Form.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function Form(props){
+
+    const { values, update, submit, errorText } = props
+    const onChange = evt => {
+        const name = evt.target.name;
+        const value = evt.target.value;
+        update(name, value);
+        console.log(name,value)
+    }
+
+    const onSubmit = evt => {
+        evt.preventDefault();
+        submit();
+      
+    }
+
+
+    return(
+    <form className='for container' onSubmit={onSubmit}>
+        <h2 className='errorText'>{errorText}</h2>
+        <div className='form-group inputs'>
+            <label>Username
+                <input
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={onChange}
+                maxLength="30"
+                placeholder="Enter a username"
+                />
+            </label>
+            <label>Email
+                <input
+                type="email"
+                name="email"
+                value={values.email}
+                onChange={onChange}
+                maxLength="30"
+                placeholder="Enter an email"
+                />
+            </label>
+            <label>Role
+            <select value={values.role} name="role" onChange={onChange}>
+                <option value="">-- Select a role --</option>
+                <option value="Frontend">Frontend</option>
+                <option value="Backend">Backend</option>
+                <option value="Full Stack">Full Stack</option>
+            </select>
+            </label>
+            <div className='submit'>
+                <button>submit</button>
+            </div>
+        </div>
+    </form>
+    )
+};
