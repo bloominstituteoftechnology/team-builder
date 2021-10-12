ead30d2682aa96a75ea82041a33ff05f3f9e3f22
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-12 14:52:10 -0400

    First commit

---
+++ team-builder/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="container">
+        <div>
+          <h1>List of team members</h1>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
