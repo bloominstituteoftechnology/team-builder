dea3e6c56fd1bad0c5920cda22ef37b7ae463020
Christian <christiansmith2394@gmail.com>
2021-10-12 16:00:35 -0400

    Broke when implementing axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import React, { useState } from 'react';
-import Form from "./Form"
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Form from "./Form";
+import "./App.css";
+import axios from 'axios'
 
 const initialTeamValue = {
-  name: '',
-  email: '',
-  role: ''
-}
+  name: "",
+  email: "",
+  role: "",
+};
 
 function App() {
+  const [teamMembers, setTeamMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialTeamValue)
+  const [errorText, setErrorText] = useState("");
 
-  const [teamMembers, setTeamMembers] = useState(initialTeamValue)
+  const updateForm = (inputName, inputValue) => {
+    setTeamMembers({ ...teamMembers, [inputName]: inputValue });
+  };
 
+  const submitForm = () => {
+    const newTeamMember = {
+      name: teamMembers.name.trim,
+      email: teamMembers.email.trim,
+      role: teamMembers.role,
+    };
+
+  if (!newTeamMember.name || !newTeamMember.email || newTeamMember.role) {
+    setErrorText('You must complete all fields')
+    return
+  }
+
+  axios
+    .post("fakeapi.com", newTeamMember)
+    .then((res) => {
+      setTeamMembers([res.data, teamMembers])
+      setFormValues(initialTeamValue)
+      setErrorText('')
+    })
+    .catch((err) => {
+    console.error(err)
+    })
+
+    useEffect(() => {
+      axios.get("fakeapi.com").then((res) => setTeamMembers(res.data))
+    })
+}
   return (
     <div className="App">
       <h2>Hello World!</h2>
+      <Form
+        values={teamMembers}
+        update={updateForm}
+        submit={submitForm}
+        errorText={errorText}
+      />
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default function Form(props) {
+  const { values, update, submit, errorText } = props;
+
+  const onChange = (evt) => {
+    const { name, value } = evt.target;
+
+    update(name, value);
+  };
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={onChange}
+            placeholder="Enter your name"
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={onChange}
+            placeholder="Enter your email"
+          />
+        </label>
+        <lable>
+          Role
+          <select value={values.role} name="role" onChange={onChange}>
+            <option value="">--Choose a role!--</option>
+            <option value="front-end">Frnt-end Developer</option>
+            <option value="back-end">Back-End Developer</option>
+            <option value="designer">UI Designer</option>
+            <option value="hacker">Ethical Hacker</option>
+            <option value="freelancer">Freelancer</option>
+          </select>
+        </lable>
+        <div className='submit'>
+            <button>Submit Details</button>
+        </div>
+      </form>
+    </div>
+  );
+}
