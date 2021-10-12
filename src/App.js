d65052f0bb521e1828ac5a81b7cb803659578d6f
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-10-12 15:31:37 -0400

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
-import react from 'react';
+import react, { useState } from 'react';
+import Form from './Form';
+
 
 function App() {
+  const initialFormValues = {
+    ///// TEXT INPUTS /////
+    name: '',
+    email: '',
+    ///// DROPDOWN /////
+    role: '',
+  }
+
+const [formValues, setFormValues] = useState(initialFormValues)
+const [members, setMembers] = useState([]);
+
+
+const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]:inputValue})
+}
+
+const submitForm = () => {
+  
+    const newMember = {
+      name: formValues.name.trim(),
+      email:formValues.email.trim(),
+      role: formValues.role
+    }
+
+    setMembers([newMember, ...members])
+
+}
+
+
   return (
+
     <div className="App">
-      <h2>Hello World</h2>
+      <Form 
+      values={formValues}
+      submit={submitForm}
+      update={updateForm}
+      />
+     
+      <h2>Team Members</h2>
+      <hr></hr>
+      {
+        members.map(mem => {
+          return(
+            <div>
+              <p>Name: {mem.name}</p>
+              <p>Email: {mem.email}</p>
+              <p>Role: {mem.role}</p>
+              <hr></hr>
+            </div>
+          )
+        })
+      }
     </div>
+    
+
   );
 }
 
