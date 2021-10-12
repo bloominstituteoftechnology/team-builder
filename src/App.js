89083ec301a568fa43a52b74d08433628b5b5bc5
Christian <christiansmith2394@gmail.com>
2021-10-12 14:53:30 -0400

    Structuring App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from "./Form"
 import './App.css';
 
+const initialTeamValue = {
+  name: '',
+  email: '',
+  role: ''
+}
+
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState(initialTeamValue)
+
   return (
     <div className="App">
       <h2>Hello World!</h2>
