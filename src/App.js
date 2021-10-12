ed7f56b69a1751a4ca2f356ac118ccb7e14e9874
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-10-12 13:43:23 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import react from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hello world</h2>
+    </div>
+  );
+}
+
+export default App;
