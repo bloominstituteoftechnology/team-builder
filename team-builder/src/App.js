7b1cb4f4fb7047a3a76531bc8c81fa13bf443352
brad <bradley.michaels@outlook.com>
2021-10-13 01:13:28 -0400

    cant get submit working correctly

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,8 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Form from './Form'
 import Turtle from './Turtle';
+import axios from '../node_modules/axios'
 function App() {
+
+  const initialFormValues = {
+    
+    name: '',
+    email: '',
+    role: '',
+    id: 0,
+  }
 
   const [teamTurtles, setTeamTurtles] = useState([
     {
@@ -31,24 +40,55 @@ function App() {
     },
 
   ])
+  const [errorText, setErrorText] = useState("")
+  const [formValues, setFormValues] = useState(initialFormValues);
 
-  const addTurtle = inputT => {
+  const updateForm = (inputName, inputValue) => {
+
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  }
+  const addTurtle = () => {
     const newTurtle = {
-      name: inputT.name,
-      email: inputT.email,
-      role: inputT.role,
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
       id: teamTurtles.length,
 
     }
-    setTeamTurtles([teamTurtles]);
-    
+    if (!newTurtle.name || !newTurtle.email || !newTurtle.role) {
+      setErrorText("You've gotta BE A TURTLE TO ENTER INFO");
+      return;
+    }
+    //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
+    axios.post(formValues, newTurtle)
+      .then(res => {
+        setTeamTurtles([res.data, ...teamTurtles]);
+        setFormValues(initialFormValues);
+        setErrorText("");
+      }).catch(err => {
+        console.error(err);
+      })
+    //  d) also on success clear the form
   }
+  useEffect(() => {
+    axios.get(teamTurtles).then(res => setTeamTurtles(res.data.data))
+  }, [])
+  
+
+
+
+
+
 
 console.log(teamTurtles);
 
 return (
   <div>
-    <Form />
+    <Form 
+    values = {formValues}
+    update = {updateForm}
+    submit = {addTurtle}
+    />
     {
         teamTurtles.map(turtle => {
           return (
