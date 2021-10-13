9d7b741a344f8b98e8aa931bb641c6cbcb9c81ab
arsalan rahimi <years1373@yahoo.com>
2021-10-12 23:39:45 -0700

    completed

---
+++ team-builder/src/component/Form.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+    const [state, setState] = useState({
+        name:"",
+        password:"",
+        email:""
+    })
+
+    const handleChange= (event) => { 
+const {name,value} = event.target
+setState ( {...state, [name]:value})
+    }
+const handleSubmit =(event) => { 
+console.log(event);
+event.preventDefault ()
+}
+
+
+    return (
+        <div>
+            {state.name}
+            <form onSubmit= {handleSubmit}>
+)
+                <label htmlFor="name" >name</label>
+                <input onChange={handleChange} value={state.name} id="name" type="text" name="name" />
+                <br/>
+                <label htmlFor="password">password</label>
+                <input onChange={handleChange} value={state.password} id="password" type="password" name="password"/>
+                <br/>
+                <label htmlFor="e-mail">e-mail</label>
+                <input onChange={handleChange} value={state.email} id="e-mail" type="email" name="email"/>
+                <br/>
+                <button>Submit here</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
