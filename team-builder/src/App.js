7bd50527dbfab8b4e863d02182f6aa76fd98345b
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-09 17:37:58 -0700

    fixed errors

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {useState } from 'react';
 import './App.css';
+import Form from './Form';
 
 function App() {
+
+  const [teamMembers, setTeamMembers] = useState([]); 
+  const [values, setValues] = useState({name:'', email:'', role:''});
+  
+  const onSubmit = () => {
+    setTeamMembers([values,...teamMembers]);
+    setValues({name:'',email:'', role:'' });
+  }
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Team Builder App!</h1>
+      <Form
+      values ={values}
+      change={onChange}
+      submit={onSubmit}
+      />
+  
+    {teamMembers.map((member, idx)=>{
+    return (
+      <div key={idx}>
+      {member.email}, {member.name}, {member.role}
+      </div>
+    )
+    })}
     </div>
-  );
-}
+    );
+  }
 
 export default App;
---
+++ team-builder/src/form.js
@@ -0,0 +1,41 @@
+import  React from 'react';
+
+
+const Form = (props) => {
+const handleChange = event =>{
+    const { name,value } = event.target;
+    props.change(name,value)
+}
+const handleSubmit = event => {
+    event.preventDefault();
+    props.submit();
+}
+return (
+    <form onSubmit={handleSubmit}>
+        <label>Name
+        <input placeholder= 'Enter name here!'
+        value={props.values.name}
+        name='name'
+        onChange={handleChange}
+        />
+        </label>
+        <label>Email
+        <input placeholder='Enter email here!'
+        value={props.values.email}
+        name='email'
+        onChange={handleChange}/>
+
+    </label>
+    <label>Role
+        <input
+        placeholder='Enter role here!'
+        value={props.values.role}
+        name='role'
+   onChange={handleChange}/>
+    </label>
+    </form>
+    )
+}
+
+export default  Form
+   
