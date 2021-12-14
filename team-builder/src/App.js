a0e0e015b4225a2973e1a354f5300e2ca7e438a1
lomelo_x <aalomelo@gmail.com>
2021-12-14 16:57:15 -0600

    Completed Task 2: Build Your Form'

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
-import React { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import TeamMember from './TeamMember';
+import TeamMemberForm from './TeamMemberForm';
+
+const initialFormValues = {
+  username: '',
+  email: '',
+  role: '',
+}
 
 function App() {
+  
+  const [members, setMembers] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [error, setError] = useState('')
+  
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({...formValues, [inputName]: inputValue})
+  }
+
+  const submitForm = () => {
+    const newMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+
+    if (!newMember.username || !newMember.email || !newMember.role) {
+      setError('All fields are required!')
+    } else {
+      setMembers([ newMember, ...members ]);
+      setFormValues(initialFormValues);
+      setError("")
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Team Members
+        </h1>
+        <h2>
+          {error}
+        </h2>
+      
+      <TeamMemberForm 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+      />
+      {
+        members.map(member => {
+          return (
+            <TeamMember key={member.id} details={member}/>
+          )
+        })
+      }
       </header>
     </div>
   );
