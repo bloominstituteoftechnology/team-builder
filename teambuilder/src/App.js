875807e5fbdd6d167a99eedeaad7d1ee36113cd0
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-12-14 23:04:54 -0500

    update on the project

--- teambuilder/src/App.js
+++ teambuilder/src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const nameList = [
+  {fName : "Shamir", gender: "male"},
+  {fName : "Tiana", gender: "female"},
+  {fName : "Debs", gender: "female"}
+]
+
+const initialFormValues = { fName: '', gender: ''};
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [names, setNames] = useState(nameList);
+
+  const change = (evt)=>{
+    const { value, name } = evt.target;
+    setFormValues({...formValues, [name]: value})
+  }
+
+  const submit = ( evt ) =>{
+    evt.preventDefault();
+    const newPerson =  {
+      fName: formValues.fName.trim(),
+      gender: formValues.gender.trim(),
+    }
+    setNames(names.concat(newPerson))
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Simple Form App </h1>
+      {names.map((name, idx)=>(
+        <div key={idx}>
+          {name.fName} is a {name.gender}
+        </div>
+  ))}
+      <form onSubmit={submit}>
+        <input value={formValues.fName} onChange={change} name='name' type='text'/>
+        <input value={formValues.gender} onChange={change} name='gender' type='text'/>
+        <button> Press </button>
+      </form>
+
     </div>
   );
 }
