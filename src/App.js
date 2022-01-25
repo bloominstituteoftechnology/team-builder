899a7e4ef7ede67b26224d9f0d2193a69f102f32
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-01-25 13:49:44 -0800

    Added componets folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import axios from 'axios';
+import MemberForm from './Componets/MemberForm';
+// import { Route, Link } from 'react-router-dom';
+import Member from './Componets/Member';
+
+
+
+const initialFormValues = {
+  memberName: '',
+  email: '',
+  role: ''
+}
 
 function App() {
+  const [members, setMembers] = useState([]);
+  const[formValues, setFormValues] = useState(initialFormValues);
+  const [error, setError] = useState('');
+
+  const updateForm = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  }
+
+  const submitForm = () => {
+    // event.preventDefault();
+    const newMember = {
+      memberName: formValues.memberName.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role
+    }
+    if(!newMember.memberName || !newMember.email || !newMember.role){
+      setError('All Fields Required');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{error}</h1>
+      <MemberForm 
+       values={formValues}
+       update={updateForm}
+       submit={submitForm}
+      />
+
+    {
+    members.map(member  => {
+      return(
+        <Member key={member.id} details={member} />
+     )
+      })
+     }   
     </div>
   );
 }
