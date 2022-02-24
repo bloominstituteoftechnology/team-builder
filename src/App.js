57e0fd521a27b7be9a7dda724b0fdbe46b36f384
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-02-24 09:03:54 -0800

    almost done followed soution, broken

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './Componenets/Form';
 
 function App() {
+
+  const [members, setMembers] = useState([]);
+  const [values, setValues] = useState({name: '', email: '', role: ''});
+
+  const onSubmit = () => {
+    setMembers([values, ...members]);
+  }
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value});
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Team Builder App Yo</h1> 
+     <Form
+     values={values}
+     change={onChange}
+     submit={onSubmit}
+    />
+    {members.map(member, idx) => {
+      return (
+        <div key={(idx)}>
+          {member.email}, {member.name}, {member.role} 
+        </div>
+      )    
+    })}
     </div>
   );
 }
