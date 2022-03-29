d21f2a930f2ed1b00a2aa4971b49430bd9cc0e8e
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-03-29 16:56:49 -0400

    Created Form Component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Team Builder App</h1>
+    </div>
+  );
+}
+
+export default App;
